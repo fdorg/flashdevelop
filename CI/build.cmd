5f7ba497e38ee8b5855f05086526a51cac44d257
Mika Palmu <meychi@gmail.com>
2013-10-16 11:37:26 +0300

    More CI fixing...

--- CI/build.cmd
+++ CI/build.cmd
@@ -5,13 +5,14 @@ git reset HEAD --hard
 
 :build
 msbuild PluginCore\PluginCore.csproj /p:Configuration=Release /p:Platform=x86
-call SetVersion
+copy CI\files\AssemblyInfo.cs FlashDevelop\Properties\AssemblyInfo.cs
 msbuild FlashDevelop\FlashDevelop.csproj /p:Configuration=Release /p:Platform=x86
 msbuild FlashDevelop.sln /p:Configuration=Release /p:Platform=x86
 
 :: remove debug files
 cd FlashDevelop\Bin\Debug
-del /S *.vshost.exe*
+del /S *.vshost.exe
+del /S *.vshost.exe.config
+del /S *.vshost.exe.manifest
 
-:: exit with error if needed
-if %errorlevel% neq 0 exit /b %errorlevel%
+pause
