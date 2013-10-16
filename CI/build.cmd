c0e25018ecbd9aa9b80b2b0018adc165e6c1250c
Mika Palmu <meychi@gmail.com>
2013-10-16 12:20:17 +0300

    CI fixing....

--- CI/build.cmd
+++ CI/build.cmd
@@ -1,3 +1,7 @@
+:: Additionals for cmd only build
+:: set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319\
+:: cd ..
+
 :: reset local files to original state
 @if "%1" neq "full" goto build
 del /S /Q FlashDevelop\Bin\Debug 
@@ -5,12 +9,6 @@ git reset HEAD --hard
 
 :build
 msbuild PluginCore\PluginCore.csproj /p:Configuration=Release /p:Platform=x86
-copy CI\files\AssemblyInfo.cs FlashDevelop\Properties\AssemblyInfo.cs
-msbuild FlashDevelop\FlashDevelop.csproj /p:Configuration=Release /p:Platform=x86
+call SetVersion
 msbuild FlashDevelop.sln /p:Configuration=Release /p:Platform=x86
 
-:: remove debug files
-cd FlashDevelop\Bin\Debug
-del /S *.vshost.exe
-del /S *.vshost.exe.config
-del /S *.vshost.exe.manifest
