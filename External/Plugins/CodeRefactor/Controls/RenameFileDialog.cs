3950449f125127b61168956d89f2b5a193395e8f
Mika Palmu <meychi@gmail.com>
2013-10-17 13:56:06 +0300

    RenameFileDialog localized and some cleanup...

--- External/Plugins/CodeRefactor/Controls/RenameFileDialog.cs
+++ External/Plugins/CodeRefactor/Controls/RenameFileDialog.cs
@@ -3,32 +3,164 @@ using System;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using PluginCore.Localization;
 
 namespace CodeRefactor.Controls
 {
     public partial class RenameFileDialog : Form
     {
         static readonly Regex re_validFirstChar = new Regex(@"^[A-Z_$]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
         private string oldFullPath;
         private string oldDirectoryName;
         private string oldName;
         private string ext;
 
+        #region Windows Form Designer generated code
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ExitButton;
+        public System.Windows.Forms.Button OKButton;
+        public System.Windows.Forms.TextBox NewName;
+        public System.Windows.Forms.CheckBox UpdateReferences;
+        private System.Windows.Forms.Label WarningLabel;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.NewName = new System.Windows.Forms.TextBox();
+            this.UpdateReferences = new System.Windows.Forms.CheckBox();
+            this.WarningLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(10, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "New name:";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ExitButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.ExitButton.Location = new System.Drawing.Point(395, 75);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Cancel";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // OKButton
+            // 
+            this.OKButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.OKButton.Location = new System.Drawing.Point(314, 75);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 2;
+            this.OKButton.Text = "Ok";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // NewName
+            // 
+            this.NewName.Location = new System.Drawing.Point(77, 16);
+            this.NewName.Name = "NewName";
+            this.NewName.Size = new System.Drawing.Size(393, 20);
+            this.NewName.TabIndex = 0;
+            this.NewName.WordWrap = false;
+            // 
+            // UpdateReferences
+            // 
+            this.UpdateReferences.AutoSize = true;
+            this.UpdateReferences.Checked = true;
+            this.UpdateReferences.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.UpdateReferences.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.UpdateReferences.Location = new System.Drawing.Point(13, 47);
+            this.UpdateReferences.Name = "UpdateReferences";
+            this.UpdateReferences.Size = new System.Drawing.Size(114, 17);
+            this.UpdateReferences.TabIndex = 1;
+            this.UpdateReferences.Text = "Update references";
+            this.UpdateReferences.UseVisualStyleBackColor = true;
+            // 
+            // WarningLabel
+            // 
+            this.WarningLabel.AutoSize = true;
+            this.WarningLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.WarningLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.WarningLabel.Location = new System.Drawing.Point(10, 76);
+            this.WarningLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.WarningLabel.Name = "WarningLabel";
+            this.WarningLabel.Size = new System.Drawing.Size(54, 13);
+            this.WarningLabel.TabIndex = 12;
+            this.WarningLabel.Text = "Warnign";
+            this.WarningLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // RenameFileDialog
+            // 
+            this.AcceptButton = this.OKButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.ExitButton;
+            this.ClientSize = new System.Drawing.Size(484, 104);
+            this.Controls.Add(this.WarningLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.NewName);
+            this.Controls.Add(this.UpdateReferences);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RenameFileDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Rename Class";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
         public RenameFileDialog()
         {
+            this.Owner = (Form)PluginBase.MainForm;
+            this.Font = PluginBase.Settings.DefaultFont;
             InitializeComponent();
             OKButton.DialogResult = DialogResult.OK;
         }
 
         internal void ShowDialogFor(string path)
         {
-            if (string.IsNullOrEmpty(path))
-                Close();
+            if (string.IsNullOrEmpty(path)) Close();
             else
             {
                 UpdateReferences.Checked = true;
-
                 SetOldFullPath(path);
                 ShowDialog();
                 OnNewNameChanged();
@@ -41,7 +173,6 @@ namespace CodeRefactor.Controls
             oldDirectoryName = Path.GetDirectoryName(path);
             oldName = Path.GetFileNameWithoutExtension(path);
             ext = Path.GetExtension(path);
-
             NewName.TextChanged += OnNewNameChanged;
             NewName.Text = oldName;
             NewName.SelectAll();
@@ -49,29 +180,24 @@ namespace CodeRefactor.Controls
 
         private void OnNewNameChanged(object sender = null, EventArgs e = null)
         {
+            bool canRename = false;
+            bool withImage = false;    
             string newName = NewName.Text;
             string newFileName = string.Concat(newName, ext);
             string newFullPath = Path.Combine(oldDirectoryName, newFileName);
-            bool canRename = false;
-            bool withImage = false;
-
             WarningLabel.AutoSize = true;
-
             if (string.IsNullOrEmpty(newName) || newName == oldName)
             {
-                //TODO: Localize
-                WarningLabel.Text = "Enter a new name.";
+                WarningLabel.Text = TextHelper.GetString("Label.EnterName");
             }
             else if (!re_validFirstChar.IsMatch(newName))
             {
-                //TODO: Localize
-                WarningLabel.Text = "New name is not a valid indentifier.";
+                WarningLabel.Text = TextHelper.GetString("Label.NotAValidId");
                 withImage = true;
             }
             else if (File.Exists(newFullPath))
             {
-                //TODO: Localize
-                WarningLabel.Text = string.Format("Name {0} is already taken", newFileName);
+                WarningLabel.Text = string.Format(TextHelper.GetString("Label.NameTaken"), newFileName);
                 withImage = true;
             }
             else
@@ -79,7 +205,6 @@ namespace CodeRefactor.Controls
                 WarningLabel.ResetText();
                 canRename = true;
             }
-
             if (withImage)
             {
                 int width = WarningLabel.Width;
@@ -88,11 +213,10 @@ namespace CodeRefactor.Controls
                 WarningLabel.Width = width + WarningLabel.Image.Width;
                 WarningLabel.Height = WarningLabel.Image.Height;
             }
-            else
-                WarningLabel.Image = null;
-
+            else WarningLabel.Image = null;
             OKButton.Enabled = canRename;
         }
 
     }
+
 }
