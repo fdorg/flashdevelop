8bbed618693eeec551c9fc532eb5a3389c57301a
Mika Palmu <meychi@gmail.com>
2013-10-23 10:38:37 +0300

    CI improvements...

--- CI/deploy.ps1
+++ CI/deploy.ps1
@@ -17,5 +17,7 @@ $pass = $variables["SecurePass"]
 foreach($artifact in $artifacts.values)
 {
     Write-Output "Upload artifact: $($artifact.name)"
-    ncftpput.exe -u "$login" -p "$pass" -C ftp.flashdevelop.org "$($artifact.path)" "downloads/builds/$($artifact.name)"
+    $ext = [System.IO.Path]::GetExtension($artifact.name)
+    $name = [System.IO.Path]::GetFileNameWithoutExtension($artifact.name)
+    ncftpput.exe -u "$login" -p "$pass" -C ftp.flashdevelop.org "$($artifact.path)" "downloads/builds/$name-$projectBuildNumber$ext";
 }
