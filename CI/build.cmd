c570d648dad723935fc8e31958aef2864b195cd7
Mika Palmu <meychi@gmail.com>
2013-10-23 09:46:51 +0300

    CI full build, testing...

--- CI/build.cmd
+++ CI/build.cmd
@@ -1,7 +1,6 @@
-:: Local build additions..
+:: Local tweaks
 ::set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319\
 ::set PATH=%PATH%;C:\Program Files (x86)\Git\bin\
-::set PATH=%PATH%;C:\Program Files (x86)\NSIS\
 ::set PATH=%PATH%;C:\Program Files\7-Zip\
 ::cd ..
 
@@ -27,7 +26,7 @@ msbuild FlashDevelop.sln /p:Configuration=Release /p:Platform=x86
 if %errorlevel% neq 0 goto :error
 
 :: Create the installer
-:: makensis FlashDevelop\Installer\Installer.nsi
+CI\nsis\makensis.exe FlashDevelop\Installer\Installer.nsi
 
 :: Check for nsis errors
 if %errorlevel% neq 0 goto :error
