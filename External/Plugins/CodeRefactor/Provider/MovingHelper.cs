27b1cf020faaa8d4d94a3bc17923b8b153c722bc
slavara <ratdg@ya.ru>
2013-12-13 21:03:18 +0400

    Add queue of move commands .

---
+++ External/Plugins/CodeRefactor/Provider/MovingHelper.cs
@@ -0,0 +1,54 @@
+﻿using CodeRefactor.Commands;
+using PluginCore.Managers;
+using System;
+using System.Collections.Generic;
+
+namespace CodeRefactor.Provider
+{
+    class MovingHelper
+    {
+        private static List<Dictionary<string, string>> queue = new List<Dictionary<string, string>>();
+        private static Move currentCommand;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void AddToQueue(Dictionary<string, string> oldPathToNewPath)
+        {
+            queue.Add(oldPathToNewPath);
+            if (currentCommand == null) MoveFirst();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void MoveFirst()
+        {
+            try
+            {
+                Dictionary<string, string> oldPathToNewPath = queue[0];
+                queue.Remove(oldPathToNewPath);
+                currentCommand = new Move(oldPathToNewPath);
+                currentCommand.OnRefactorComplete += OnRefactorComplete;
+                currentCommand.Execute();
+            }
+            catch(Exception ex)
+            {
+                queue.Clear();
+                currentCommand = null;
+                ErrorManager.ShowError(ex);
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void OnRefactorComplete(object sender, RefactorCompleteEventArgs<IDictionary<string, List<PluginCore.FRService.SearchMatch>>> e)
+        {
+            if (queue.Count > 0) MoveFirst();
+            else currentCommand = null;
+        }
+
+    }
+}
