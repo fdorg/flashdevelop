9be9642acbfb3575c8d98fdf05eb92c0d23f6e4e
Mika Palmu <meychi@gmail.com>
2013-12-30 17:23:47 +0200

    Named build log...

--- CI/deploy.ps1
+++ CI/deploy.ps1
@@ -27,6 +27,7 @@ foreach($artifact in $artifacts.values)
 Write-Output "Create and upload LATEST_BUILD.txt"
 $date = Get-Date
 $file = [System.IO.Path]::GetTempFileName()
+$name = [System.IO.Path]::GetFileNameWithoutExtension($artifact.name)
 $data = "Build Number: $projectBuildNumber`r`nTime: " + $date.ToUniversalTime() + " GMT"
 $data | Set-Content $file
-ncftpput.exe -u "$login" -p "$pass" -C ftp.flashdevelop.org "$file" "downloads/builds/LATEST_BUILD.txt";
+ncftpput.exe -u "$login" -p "$pass" -C ftp.flashdevelop.org "$file" "downloads/builds/$name.txt";
