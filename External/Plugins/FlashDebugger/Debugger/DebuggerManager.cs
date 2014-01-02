ffe879d75d98b954868fe9b2caae9e31fdae84d5
Mika Palmu <meychi@gmail.com>
2014-01-02 12:13:48 +0200

    Typo fix...

--- External/Plugins/FlashDebugger/Debugger/DebuggerManager.cs
+++ External/Plugins/FlashDebugger/Debugger/DebuggerManager.cs
@@ -314,7 +314,7 @@ namespace FlashDebugger
                 PanelsHelper.stackframePanel.Show();
                 PanelsHelper.pluginPanel.Show();
 				PanelsHelper.breakPointPanel.Show();
-                PanelsHelper.threadsUI.Show();
+                PanelsHelper.threadsPanel.Show();
 			}
 		}
 
@@ -340,7 +340,7 @@ namespace FlashDebugger
                 PanelsHelper.stackframePanel.Hide();
                 PanelsHelper.watchPanel.Hide();
                 PanelsHelper.immediatePanel.Hide();
-                PanelsHelper.threadsUI.Hide();
+                PanelsHelper.threadsPanel.Hide();
             }
 			PanelsHelper.pluginUI.TreeControl.Nodes.Clear();
 			PanelsHelper.stackframeUI.ClearItem();
