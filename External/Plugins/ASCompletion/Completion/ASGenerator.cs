df52dfaced3e1afa5a5cf652dfcfceeed5d8d315
slavara <ratdg@ya.ru>
2014-05-07 15:18:05 +0400

    Fix unsafe conditions

--- External/Plugins/ASCompletion/Completion/ASGenerator.cs
+++ External/Plugins/ASCompletion/Completion/ASGenerator.cs
@@ -1439,11 +1439,8 @@ namespace ASCompletion.Completion
             List<FunctionParameter> functionParameters = ParseFunctionParameters(Sci, wordPos);
 
             ASResult funcResult = ASComplete.GetExpressionType(Sci, Sci.WordEndPosition(Sci.CurrentPos, true));
-            if (funcResult.Member == null)
-            {
-                return;
-            }
-            if (funcResult != null && funcResult.InClass != null && !funcResult.InClass.Equals(inClass))
+            if (funcResult == null || funcResult.Member == null) return;
+            if (funcResult.InClass != null && !funcResult.InClass.Equals(inClass))
             {
                 AddLookupPosition();
                 lookupPosition = -1;
