7896b1caf445632a3d8eda00395f4a4fe30411a0
Mika Palmu <meychi@gmail.com>
2014-12-11 00:43:53 +0200

    Color utils added...

--- PluginCore/PluginCore/Utilities/DataConverter.cs
+++ PluginCore/PluginCore/Utilities/DataConverter.cs
@@ -75,7 +75,7 @@ namespace PluginCore.Utilities
         }
 
         /// <summary>
-        /// Converts a String to a color
+        /// Converts a String to a color (BGR order)
         /// </summary>
         public static Int32 StringToColor(String aColor)
         {
@@ -107,9 +107,17 @@ namespace PluginCore.Utilities
         }
 
         /// <summary>
-        /// Converts a color to a integer
+        /// Converts a integer (BGR order) to a color
         /// </summary>
-        public static Int32 ColorToInt32(Color color)
+        private static Color BGRToColor(Int32 bgr)
+        {
+            return Color.FromArgb((bgr >> 0) & 0xff, (bgr >> 8) & 0xff, (bgr >> 16) & 0xff);
+        }
+
+        /// <summary>
+        /// Converts a color to an integer (BGR order)
+        /// </summary>
+        public static Int32 ColorToBGR(Color color)
         {
             return TO_COLORREF(color.ToArgb() & 0x00ffffff);
         }
