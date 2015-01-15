e7eef80061c7218c50d8efaf40babe4e4079c09d
Martin Jonasson <grapefrukt@grapefrukt.com>
2015-01-15 09:59:44 +0100

    Adds a list of keywords whose lines to highlight in the Output panel fdorg/flashdevelop#563

--- External/Plugins/OutputPanel/Settings.cs
+++ External/Plugins/OutputPanel/Settings.cs
@@ -1,17 +1,56 @@
 using System;
-using System.Text;
 using System.ComponentModel;
 using System.Collections.Generic;
+using System.Drawing.Design;
+using System.ComponentModel.Design;
 using PluginCore.Localization;
 
 namespace OutputPanel
 {
+    [Serializable]
+    [DefaultProperty("Path")]
+    public class HighlightKeyword
+    {
+        private String m_Keyword;
+        private LogLevel m_Level;
+
+        public HighlightKeyword()
+        {
+            m_Keyword = "";
+        }
+
+        public HighlightKeyword(String value)
+        {
+            m_Keyword = value;
+        }
+        
+        public String Keyword
+        {
+            get { return m_Keyword; }
+            set { m_Keyword = value; }
+        }
+
+        public override String ToString()
+        {
+            return m_Keyword;
+        }
+        
+        [DefaultValue(LogLevel.Debug)]
+        [LocalizedDescription("OutputPanel.Description.LogLevel")]
+        public LogLevel Level
+        {
+            get { return m_Level; }
+            set { m_Level = value; }
+        }
+    }
+
     [Serializable]
     public class Settings
     {
         private Boolean showOnOutput = false;
         private Boolean showOnProcessEnd = false;
         private Boolean wrapOutput = false;
+        private HighlightKeyword[] keywords = new HighlightKeyword[] { };
 
         /// <summary> 
         /// Get and sets the alwaysShow
@@ -46,6 +85,32 @@ namespace OutputPanel
             set { this.wrapOutput = value; }
         }
 
+        [DisplayName("Highlighted Keywords")]
+        [LocalizedCategory("FlashDebugger.Category.Misc")]
+        [LocalizedDescription("FlashDebugger.Description.HighlightedKeywords")]
+        [Editor(typeof(ArrayEditor), typeof(UITypeEditor))]
+        public HighlightKeyword[] Keywords
+        {
+            get
+            {
+                if (keywords == null || keywords.Length == 0)
+                {
+                    keywords = new HighlightKeyword[] { };
+                }
+                return keywords;
+            }
+            set { keywords = value; }
+        }
+       
+    }
+
+    public enum LogLevel
+    {
+        Info,
+        Debug,
+        Warning,
+        Error,
+        Fatal
     }
 
 }
