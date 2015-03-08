58f85d724b8f88996e8fde9f48d2e57b66844af5
Mika Palmu <meychi@gmail.com>
2015-03-08 23:31:01 +0200

    GetLine is now fixed to match Scintilla and now return all eols, previous returned only \r. This also matches other line related methods.

--- PluginCore/ScintillaNet/ScintillaControl.cs
+++ PluginCore/ScintillaNet/ScintillaControl.cs
@@ -3214,7 +3214,6 @@ namespace ScintillaNet
 			byte[] buffer = new byte[sz + 1];
 			fixed (byte* b = buffer) SPerform(2153, (uint)line, (uint)b);
             if (sz == 0) return ""; // Empty last line
-            if (buffer[sz - 1] == 10) sz--;
 			return Encoding.GetEncoding(this.CodePage).GetString(buffer, 0, sz);
 		}
 
