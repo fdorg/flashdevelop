7ea466c0610f103096dfa83cce671eeabe28e042
Mika Palmu <meychi@gmail.com>
2015-03-13 21:28:40 +0200

    AIR & FP 17 updates...

--- PluginCore/PluginCore/PlatformData.cs
+++ PluginCore/PluginCore/PlatformData.cs
@@ -20,9 +20,9 @@ namespace PluginCore
 
         public static string ResolveFlashPlayerVersion(string lang, string platformName, string version)
         {
-            if (!SupportedLanguages.ContainsKey(lang)) return "16.0";
+            if (!SupportedLanguages.ContainsKey(lang)) return "17.0";
             var platforms = SupportedLanguages[lang.ToLower()].Platforms;
-            if (!platforms.ContainsKey(FLASHPLAYER_PLATFORM)) return "16.0";
+            if (!platforms.ContainsKey(FLASHPLAYER_PLATFORM)) return "17.0";
             var flashPlatform = platforms[FLASHPLAYER_PLATFORM];
             if (platforms.ContainsKey(platformName))
             {
@@ -40,9 +40,9 @@ namespace PluginCore
 
         public static string ResolveSwfVersion(string lang, string platformName, string version)
         {
-            if (!SupportedLanguages.ContainsKey(lang)) return "27";
+            if (!SupportedLanguages.ContainsKey(lang)) return "28";
             var platforms = SupportedLanguages[lang.ToLower()].Platforms;
-            if (!platforms.ContainsKey(FLASHPLAYER_PLATFORM) || !platforms.ContainsKey(platformName)) return "27";
+            if (!platforms.ContainsKey(FLASHPLAYER_PLATFORM) || !platforms.ContainsKey(platformName)) return "28";
             var platform = platforms[platformName];
             foreach (var platformVersion in platform.Versions)
                 if (platformVersion.Value == version)
