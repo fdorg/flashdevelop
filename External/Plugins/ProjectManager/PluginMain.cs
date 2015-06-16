182840913c0ee301448445ab3bb666ec05b002c2
Philippe Elsass <philippe.elsass@massiveinteractive.com>
2015-06-16 09:03:28 +0100

    Show ProjectPanel on firstRun (ie. when no PM settings exist)

--- External/Plugins/ProjectManager/PluginMain.cs
+++ External/Plugins/ProjectManager/PluginMain.cs
@@ -91,6 +91,7 @@ namespace ProjectManager
         private Timer buildTimer;
         private bool listenToPathChange;
         private ProjectManagerUIStatus uiStatus = ProjectManagerUIStatus.NotBuilding;
+        private bool firstRun;
 
         private ProjectTreeView Tree { get { return pluginUI.Tree; } }
         public static IMainForm MainForm { get { return PluginBase.MainForm; } }
@@ -110,7 +111,11 @@ namespace ProjectManager
         {
             Settings = new ProjectManagerSettings();
             if (!Directory.Exists(SettingsDir)) Directory.CreateDirectory(SettingsDir);
-            if (!File.Exists(SettingsPath)) this.SaveSettings();
+            if (!File.Exists(SettingsPath))
+            {
+                SaveSettings();
+                firstRun = true;
+            }
             else
             {
                 Object obj = ObjectSerializer.Deserialize(SettingsPath, Settings);
@@ -369,7 +374,8 @@ namespace ProjectManager
                     { 
                         BroadcastMenuInfo(); 
                         BroadcastToolBarInfo(); 
-                        OpenLastProject(); 
+                        OpenLastProject();
+                        if (firstRun) pluginPanel.Show();
                     });
                     break;
 
