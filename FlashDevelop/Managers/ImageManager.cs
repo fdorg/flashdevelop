daab8a048a06e3ce62b964e274bc3d3b16689ef8
Daniel Jeong <wise0704@outlook.com>
2015-12-07 15:52:25 +1300

    Updates to ImageManager
    Changes are made to ImageManager to allow fixes to the bug of image colors not being adjusted according to the theme.

--- FlashDevelop/Managers/ImageManager.cs
+++ FlashDevelop/Managers/ImageManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Drawing;
 using FlashDevelop.Helpers;
@@ -7,24 +6,23 @@ using PluginCore.Utilities;
 
 namespace FlashDevelop.Managers
 {
-    class ImageManager
+    static class ImageManager
     {
-        public static Int32 X;
-        public static Int32 Y;
-        public static Int32 Size;
-        public static Int32 Icon;
-        public static Int32 Bullet;
-        public static Int32 Padding;
-        public static Bitmap Source;
-        public static Dictionary<String, Bitmap> Cache;
+        static readonly int Size;
+        static readonly int Padding;
+        static readonly Bitmap Source;
+        static readonly Dictionary<string, ImagePair> Cache;
+        static readonly List<ImagePair> AutoAdjusted;
 
         /// <summary>
         /// Static constructor 
         /// </summary>
         static ImageManager()
         {
-            Double scale = ScaleHelper.GetScale();
-            Cache = new Dictionary<String, Bitmap>();
+            double scale = ScaleHelper.GetScale();
+            Cache = new Dictionary<string, ImagePair>();
+            AutoAdjusted = new List<ImagePair>();
+
             if (scale >= 1.5)
             {
                 Size = 32;
@@ -37,80 +35,158 @@ namespace FlashDevelop.Managers
                 Padding = 0;
                 Source = new Bitmap(FileNameHelper.Images);
             }
-            Source = (Bitmap)AdjustImage(Source);
         }
 
         /// <summary>
-        /// Adjusts the image for different themes
+        /// Composes an icon from image data.
         /// </summary>
-        public static Image AdjustImage(Image image)
+        public static Image GetComposedBitmap(string data, bool autoAdjusted)
         {
-            String style = Globals.MainForm.GetThemeValue("ImageManager.ImageSet");
-            if (style == "Bright") return ImageKonverter.ImageAdjust(image, 20, 0);
-            else if (style == "Dim") return ImageKonverter.ImageAdjust(image, -5, -2);
-            else if (style == "Dark") return ImageKonverter.ImageAdjust(image, -5, -10);
-            else if (style == "Darker") return ImageKonverter.ImageAdjust(image, -20, -20);
-            else if (style == "Black") return ImageKonverter.ImageAdjust(image, -50, -25);
-            else return image;
+            if (!Cache.ContainsKey(data))
+            {
+                int x, y, icon, bullet, rx, ry;
+                var composed = new Bitmap(Size, Size);
+                var destination = Graphics.FromImage(composed);
+                var destRect = new Rectangle(Padding, Padding, Size - (Padding * 2), Size - (Padding * 2));
+
+                ProcessImageData(data, out x, out y, out icon, out bullet);
+                destination.Clear(Color.Transparent);
+
+                if (icon >= 0)
+                {
+                    rx = (icon % 16) * Size;
+                    ry = (icon / 16) * Size;
+                    destination.DrawImage(Source, destRect, new Rectangle(rx, ry, Size, Size), GraphicsUnit.Pixel);
+                }
+                if (bullet >= 0)
+                {
+                    rx = (bullet % 16) * Size;
+                    ry = (bullet / 16) * Size;
+                    destRect.X += (Size == 32) ? x * 2 : x;
+                    destRect.Y += (Size == 32) ? y * 2 : y;
+                    destination.DrawImage(Source, destRect, new Rectangle(rx, ry, Size, Size), GraphicsUnit.Pixel);
+                }
+
+                composed = ScaleHelper.Scale(composed);
+                Cache[data] = GetAutoAdjustedImagePair(composed);
+            }
+
+            return autoAdjusted ? Cache[data].Adjusted : Cache[data].Original;
         }
 
         /// <summary>
-        /// Composes an icon from Image data
+        /// Gets a copy of the image that changes color according to the theme.
         /// </summary>
-        public static Bitmap GetComposedBitmap(String data)
+        public static Image GetAutoAdjustedImage(Image image)
         {
-            if (Cache.ContainsKey(data))
+            return GetAutoAdjustedImagePair(image).Adjusted;
+        }
+
+        /// <summary>
+        /// Adjust colors of all cached images.
+        /// </summary>
+        public static void AdjustAllImages()
+        {
+            string style = Globals.MainForm.GetThemeValue("ImageManager.ImageSet");
+            int saturation, brightness;
+            GetImageAdjustments(style, out saturation, out brightness);
+            
+            foreach (var imagePair in AutoAdjusted)
             {
-                return Cache[data];
+                ImageKonverter.ImageAdjust(imagePair.Original, imagePair.Adjusted, saturation, brightness);
             }
-            ProcessImageData(data);
-            Bitmap composed = new Bitmap(Size, Size);
-            Graphics destination = Graphics.FromImage(composed);
-            destination.Clear(Color.Transparent);
-            Int32 rx; Int32 ry;
-            if (Icon >= 0)
-            {
-                rx = (Icon % 16) * Size;
-                ry = (Icon / 16) * Size;
-                destination.DrawImage(Source, new Rectangle(Padding, Padding, Size - (Padding * 2), Size - (Padding * 2)), new Rectangle(rx, ry, Size, Size), GraphicsUnit.Pixel);
-            }
-            if (Bullet >= 0)
-            {
-                rx = (Bullet % 16) * Size;
-                ry = (Bullet / 16) * Size;
-                X = (Size == 32) ? X * 2 : X;
-                Y = (Size == 32) ? Y * 2 : Y;
-                destination.DrawImage(Source, new Rectangle(X + Padding, Y + Padding, Size - (Padding * 2), Size - (Padding * 2)), new Rectangle(rx, ry, Size, Size), GraphicsUnit.Pixel);
-            }
-            composed = ScaleHelper.Scale(composed);
-            Cache[data] = composed;
-            return composed;
         }
 
         /// <summary>
         /// Processes data from "icon|bullet|x|y" or just index
         /// </summary>
-        private static void ProcessImageData(String data)
+        static void ProcessImageData(string data, out int x, out int y, out int icon, out int bullet)
         {
-            X = Y = 0;
-            Icon = Bullet = -1;
+            x = y = 0;
+            icon = bullet = -1;
             if (string.IsNullOrEmpty(data)) return;
-            String[] par = data.Split('|');
-            if (par.Length > 0)
+
+            string[] args = data.Split('|');
+            if (args.Length == 0 || !int.TryParse(args[0], out icon)) return;
+            if (args.Length == 1 || !int.TryParse(args[1], out bullet)) return;
+            if (bullet < 0 || args.Length < 4) return;
+            int.TryParse(args[2], out x);
+            int.TryParse(args[3], out y);
+        }
+
+        /// <summary>
+        /// Gets an instance of <see cref="ImagePair"/> with the specified image,
+        /// that has a copy of the image that changes color according to the theme.
+        /// </summary>
+        static ImagePair GetAutoAdjustedImagePair(Image image)
+        {
+            var pair = new ImagePair(image);
+            AutoAdjusted.Add(pair);
+
+            string style = Globals.MainForm.GetThemeValue("ImageManager.ImageSet");
+            int saturation, brightness;
+
+            if (GetImageAdjustments(style, out saturation, out brightness))
             {
-                Int32.TryParse(par[0], out Icon);
-                if (par.Length > 1)
-                {
-                    Int32.TryParse(par[1], out Bullet);
-                    if (Bullet >= 0 && par.Length == 4)
-                    {
-                        Int32.TryParse(par[2], out X);
-                        Int32.TryParse(par[3], out Y);
-                    }
-                }
+                pair.Adjusted = new Bitmap(image.Width, image.Height);
+                ImageKonverter.ImageAdjust(image, pair.Adjusted, saturation, brightness);
+            }
+            else pair.Adjusted = new Bitmap(image);
+
+            return pair;
+        }
+
+        /// <summary>
+        /// Gets the appropriate color adjustment components.
+        /// </summary>
+        static bool GetImageAdjustments(string style, out int saturation, out int brightness)
+        {
+            switch (style)
+            {
+                case "Bright": saturation =  20; brightness =   0; return true;
+                case "Dim":    saturation =  -5; brightness =  -2; return true;
+                case "Dark":   saturation =  -5; brightness = -10; return true;
+                case "Darker": saturation = -20; brightness = -20; return true;
+                case "Black":  saturation = -50; brightness = -25; return true;
+                default:       saturation =   0; brightness =   0; return false;
             }
         }
 
+        /// <summary>
+        /// A pair of images used for tracking original and adjusted.
+        /// </summary>
+        class ImagePair
+        {
+            /// <summary>
+            /// The original image.
+            /// </summary>
+            public Image Original;
+
+            /// <summary>
+            /// The copy of <see cref="Original"/> that changes color according to the theme.
+            /// </summary>
+            public Image Adjusted;
+
+            /// <summary>
+            /// Creates an instance of <see cref="ImagePair"/>.
+            /// </summary>
+            /// <param name="original"><see cref="Original"/></param>
+            public ImagePair(Image original)
+            {
+                Original = original;
+            }
+
+            /// <summary>
+            /// Creates an instance of <see cref="ImagePair"/>.
+            /// </summary>
+            /// <param name="original"><see cref="Original"/></param>
+            /// <param name="adjusted"><see cref="Adjusted"/></param>
+            public ImagePair(Image original, Image adjusted)
+            {
+                Original = original;
+                Adjusted = adjusted;
+            }
+        }
     }
 
 }
