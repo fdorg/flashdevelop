fc88f9f847a85a9cdd6f335e4f8d0dbe0d8c7bd8
Mika Palmu <meychi@gmail.com>
2015-12-12 00:49:44 +0200

    CI scripts for HaxeDevelop...

---
+++ CI/build-hx.cmd
@@ -0,0 +1,52 @@
+:: Builds the binary on the server for CI
+
+:: Set paths
+set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319\
+set PATH=%PATH%;C:\Program Files (x86)\Git\bin\
+set PATH=%PATH%;C:\Program Files (x86)\NSIS
+set PATH=%PATH%;C:\Program Files\7-Zip\
+
+:: Reset bin files
+git clean -f -x -d FlashDevelop\Bin\Debug
+
+:: Copy distro files
+xcopy Distros\HaxeDevelop /s /e /y
+
+:: Remove bad files
+del FlashDevelop\Bin\Debug\FlashDevelop.exe.config
+
+:: Check for build errors
+if %errorlevel% neq 0 goto :error
+
+:: Build the PluginCore
+msbuild PluginCore\PluginCore.csproj /p:Configuration=Release /p:Platform=x86
+
+:: Check for build errors
+if %errorlevel% neq 0 goto :error
+
+:: Extract version from HEAD
+call SetVersion.bat
+
+:: Build the solution
+msbuild FlashDevelop.sln /p:Configuration=Release /p:Platform=x86
+
+:: Check for build errors
+if %errorlevel% neq 0 goto :error
+
+:: Create the installer
+makensis FlashDevelop\Installer\Installer.nsi
+
+:: Check for nsis errors
+if %errorlevel% neq 0 goto :error
+
+:: Create the archive
+7z a -tzip FlashDevelop\Installer\Binary\HaxeDevelop.zip .\FlashDevelop\Bin\Debug\* -xr!.empty
+
+:: Check for 7zip errors
+if %errorlevel% neq 0 goto :error
+
+:: Done
+exit
+
+:error
+exit -1
---
+++ CI/deploy-hx.ps1
@@ -0,0 +1,41 @@
+# Deploys the built binaries to the FD.org server
+
+Param (
+    $variables = @{},   
+    $artifacts = @{},
+    $scriptPath,
+    $buildFolder,
+    $srcFolder,
+    $outFolder,
+    $tempFolder,
+    $projectName,
+    $projectVersion,
+    $projectBuildNumber
+)
+
+$login = $variables["SecureLogin"]
+$pass = $variables["SecurePass"]
+
+foreach($artifact in $artifacts.values)
+{
+    Write-Output "Upload artifact: $($artifact.name)"
+    $ext = [System.IO.Path]::GetExtension($artifact.name)
+    $name = [System.IO.Path]::GetFileNameWithoutExtension($artifact.name)
+    IF ($ext -eq ".xml")
+    {
+        # Upload appman.xml file
+        ncftpput.exe -u "$login" -p "$pass" -C ftp.flashdevelop.org "$($artifact.path)" "$name$ext";
+    }
+    ELSE
+    {
+        # Upload the installer and the zip archive
+        ncftpput.exe -u "$login" -p "$pass" -C ftp.flashdevelop.org "$($artifact.path)" "downloads/builds/$name$ext";
+    }
+}
+
+Write-Output "Create and upload build info."
+$date = Get-Date
+$file = [System.IO.Path]::GetTempFileName()
+$data = "Build: $projectVersion`r`nTime: " + $date.ToUniversalTime() + " GMT"
+$data | Set-Content $file
+ncftpput.exe -u "$login" -p "$pass" -C ftp.flashdevelop.org "$file" "downloads/builds/HaxeDevelop-$env:APPVEYOR_REPO_BRANCH.txt";
