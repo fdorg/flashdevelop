54c65f7ade92e626692d9327f760c7cc5af3c67f
Mika Palmu <meychi@gmail.com>
2015-12-11 23:09:33 +0200

    Initial setup...

---
+++ CI/buildl-hx.cmd
@@ -0,0 +1,56 @@
+:: Builds the binary locally and launches the installer
+
+:: Set paths
+set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319\
+set PATH=%PATH%;C:\Program Files\Git\bin\
+set PATH=%PATH%;C:\Program Files (x86)\Git\bin\
+set PATH=%PATH%;C:\Program Files\7-Zip\
+set PATH=%PATH%;C:\Program Files (x86)\7-Zip\
+set PATH=%PATH%;C:\Program Files (x86)\NSIS
+
+:: Need path up
+cd ..
+
+:: Reset bin files
+git clean -f -x -d FlashDevelop\Bin\Debug
+
+:: Copy distro files
+xcopy FlashDevelop\Distros\HaxeDevelop\ FlashDevelop\ /s /e
+
+:: Check for build errors
+if %errorlevel% neq 0 goto :error
+
+:: Build the PluginCore
+msbuild PluginCore\PluginCore.csproj /p:Configuration=Release /p:Platform=x86 /t:Rebuild
+
+:: Check for build errors
+if %errorlevel% neq 0 goto :error
+
+:: Extract version from HEAD
+call SetVersion.bat
+
+:: Build the solution
+msbuild FlashDevelop.sln /p:Configuration=Release /p:Platform=x86 /t:Rebuild
+
+:: Check for build errors
+if %errorlevel% neq 0 goto :error
+
+:: Create the installer
+makensis FlashDevelop\Installer\Installer.nsi
+
+:: Check for nsis errors
+if %errorlevel% neq 0 goto :error
+
+:: Delete old and create an new archive
+del FlashDevelop\Installer\Binary\HaxeDevelop.zip
+7z a -tzip FlashDevelop\Installer\Binary\HaxeDevelop.zip .\FlashDevelop\Bin\Debug\* -xr!.empty
+
+:: Check for 7zip errors
+if %errorlevel% neq 0 goto :error
+
+:: Done
+start FlashDevelop\Installer\Binary\HaxeDevelop.exe
+exit
+
+:error
+exit -1
---
+++ Distros/HaxeDevelop/PluginCore/PluginCore/DistroConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PluginCore
+{
+    public class DistroConfig
+    {
+        public const String DISTRIBUTION_NAME = "HaxeDevelop";
+        public const String DISTRIBUTION_DESC = "HaxeDevelop is an open source script editor.";
+        public const String DISTRIBUTION_HELP = "http://www.haxedevelop.org/";
+        public const String DISTRIBUTION_ABOUT = "HaxeDevelop logo, domain and the name are copyright of Haxe Foundataion.\r\nHaxeDevelop is a custom distribution of FlashDevelop. Development: Mika Palmu, Philippe Elsass and all helpful contributors.";
+        public const String DISTRIBUTION_COPYRIGHT = "HaxeDevelop.org 2015-2016";
+        public const String DISTRIBUTION_COMPANY = "HaxeDevelop.org";
+    }
+
+}
