8660c2d32df1e1f06d9ec7830fce0890344aa8d5
Mika Palmu <meychi@gmail.com>
2015-12-19 23:35:27 +0200

    Default start page RSS url is now in DistroConfig...

--- Distros/HaxeDevelop/PluginCore/PluginCore/DistroConfig.cs
+++ Distros/HaxeDevelop/PluginCore/PluginCore/DistroConfig.cs
@@ -11,6 +11,7 @@ namespace PluginCore
         public const String DISTRIBUTION_ABOUT = "HaxeDevelop logo, domain and the name are copyright of Haxe Foundation.\r\nHaxeDevelop is a custom distribution of FlashDevelop.\r\nDevelopment: Mika Palmu, Philippe Elsass and all helpful contributors.";
         public const String DISTRIBUTION_COPYRIGHT = "HaxeDevelop.org 2015-2016";
         public const String DISTRIBUTION_COMPANY = "HaxeDevelop.org";
+        public const String DISTRIBUTION_RSS = "http://www.flashdevelop.org/community/rss.php?f=15";
     }
 
 }
