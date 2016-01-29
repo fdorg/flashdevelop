3185630f6f4d4f3d550d13733cedd7fa8aeaeb2e
Gama11 <jensfischer95@gmail.com>
2016-01-29 23:22:56 +0100

    Missing ScintillaTest.cs, extended gitignore

---
+++ Tests/External/Plugins/ASCompletion.Tests/Completion/ScintillaTest.cs
@@ -0,0 +1,61 @@
+﻿using FlashDevelop;
+using NSubstitute;
+using NUnit.Framework;
+using PluginCore;
+using ScintillaNet;
+using ScintillaNet.Enums;
+
+namespace ASCompletion.Completion
+{
+    /// <summary>
+    /// Common base class for tests requiring a ScintillaControl.
+    /// </summary>
+    public class ScintillaTest
+    {
+        protected MainForm mainForm;
+        protected ISettings settings;
+        protected ITabbedDocument doc;
+
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            mainForm = new MainForm();
+            settings = Substitute.For<ISettings>();
+            settings.UseTabs = true;
+            settings.IndentSize = 4;
+            settings.SmartIndentType = SmartIndent.CPP;
+            settings.TabIndents = true;
+            settings.TabWidth = 4;
+            doc = Substitute.For<ITabbedDocument>();
+            mainForm.Settings = settings;
+            mainForm.CurrentDocument = doc;
+            mainForm.StandaloneMode = false;
+            PluginBase.Initialize(mainForm);
+            FlashDevelop.Managers.ScintillaManager.LoadConfiguration();
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            settings = null;
+            doc = null;
+            mainForm.Dispose();
+            mainForm = null;
+        }
+
+        protected ScintillaControl GetBaseScintillaControl()
+        {
+            return new ScintillaControl
+            {
+                Encoding = System.Text.Encoding.UTF8,
+                CodePage = 65001,
+                Indent = settings.IndentSize,
+                Lexer = 3,
+                StyleBits = 7,
+                IsTabIndents = settings.TabIndents,
+                IsUseTabs = settings.UseTabs,
+                TabWidth = settings.TabWidth
+            };
+        }
+    }
+}
