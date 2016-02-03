37a31c421b4dd2e16761f06e020da63f88928c56
Mika Palmu <meychi@gmail.com>
2016-02-03 10:13:59 +0200

    More...

--- CI/buildl.cmd
+++ CI/buildl.cmd
@@ -56,8 +56,6 @@ if %errorlevel% neq 0 goto :error
 git clean -f -x -d FlashDevelop\Bin\Debug
 
 :: Remove bad files
-del FlashDevelop\Bin\Debug\FlashDevelop.exe.config
-del FlashDevelop\Bin\Debug\FlashDevelopx64.exe.config
 del FlashDevelop\Bin\Debug\StartPage\images\*.* /Q
 for /d %%G in ("FlashDevelop\Bin\Debug\Projects\*ActionScript 3*") do rd /s /q "%%~G"
 
@@ -82,8 +80,10 @@ msbuild FlashDevelop.sln /p:Configuration=Release /p:Platform=x86 /t:Rebuild
 if %errorlevel% neq 0 goto :error
 
 :: Rename binaries
-rename FlashDevelop\Bin\Debug\FlashDevelop.exe FlashDevelop\Bin\Debug\HaxeDevelop.exe
-rename FlashDevelop\Bin\Debug\FlashDevelopx64.exe FlashDevelop\Bin\Debug\HaxeDevelopx64.exe
+ren FlashDevelop\Bin\Debug\FlashDevelop.exe HaxeDevelop.exe
+ren FlashDevelop\Bin\Debug\FlashDevelopx64.exe HaxeDevelopx64.exe
+ren FlashDevelop\Bin\Debug\FlashDevelop.exe.config HaxeDevelop.exe.config
+ren FlashDevelop\Bin\Debug\FlashDevelopx64.exe.config HaxeDevelopx64.exe.config
 
 :: Check for build errors
 if %errorlevel% neq 0 goto :error
