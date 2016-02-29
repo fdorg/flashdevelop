5a19c78f21b7748c15b3c1f03b5029e3a9d2674f
Mika Palmu <meychi@gmail.com>
2016-02-29 23:42:45 +0200

    Adding demo scripts...

---
+++ External/Scripts/InstallFlixel.cmd
@@ -0,0 +1,56 @@
+:: Script for installing OpenFL and it's dependencies.
+@echo off
+
+:check_haxelib
+
+echo Checking Haxelib...
+
+haxelib help > nul
+
+:: Check for errors
+if %errorlevel% neq 0 goto :haxelib_error
+
+echo OK
+goto :check_openfl
+
+:check_openfl
+
+echo Checking OpenFL...
+
+haxelib path openfl > nul
+
+:: Check for errors
+if %errorlevel% neq 0 goto :install_openfl
+
+echo OK
+goto :done
+
+:install_openfl
+
+echo Installing OpenFL...
+
+haxelib install openfl
+haxelib run openfl setup
+
+:: Check for errors
+if %errorlevel% neq 0 goto :install_error
+
+goto :done
+
+:haxelib_error
+
+echo Haxe is not installed. Please install Haxe first or if it's installed, restart Windows before continuing.
+pause
+exit -1
+
+:install_error
+
+echo Could not install required Haxelib library: openfl.
+pause
+exit -1
+
+:done
+
+echo OpenFL + Flixel installed successfully.
+pause
+exit 0
---
+++ External/Scripts/InstallOpenFL.cmd
@@ -0,0 +1,79 @@
+:: Script for installing Flixel and it's dependencies.
+@echo off
+
+:check_haxelib
+
+echo Checking Haxelib...
+
+haxelib help > nul
+
+:: Check for errors
+if %errorlevel% neq 0 goto :haxelib_error
+
+echo OK
+goto :check_openfl
+
+:check_openfl
+
+echo Checking OpenFL...
+
+haxelib path openfl > nul
+
+:: Check for errors
+if %errorlevel% neq 0 goto :install_openfl
+
+echo OK
+goto :check_flixel
+
+:install_openfl
+
+echo Installing OpenFL...
+
+haxelib install openfl
+haxelib run openfl setup
+
+:: Check for errors
+if %errorlevel% neq 0 goto :install_error
+
+goto :check_flixel
+
+:check_flixel
+
+echo Checking Flixel...
+
+haxelib path flixel > nul
+
+:: Check for errors
+if %errorlevel% neq 0 goto :install_flixel
+
+echo OK
+goto :done
+
+:install_flixel
+
+echo Installing Flixel...
+
+haxelib install flixel
+
+:: Check for errors
+if %errorlevel% neq 0 goto :install_error
+
+goto :done
+
+:haxelib_error
+
+echo Haxe is not installed. Please install Haxe first or if it's installed, restart Windows before continuing.
+pause
+exit -1
+
+:install_error
+
+echo Could not install required Haxelib libraries: openfl or flixel.
+pause
+exit -1
+
+:done
+
+echo OpenFL installed successfully.
+pause
+exit 0
