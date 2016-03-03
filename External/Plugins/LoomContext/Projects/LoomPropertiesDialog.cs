d90f01337e36a444f68f4c30f41ab3f638daa606
SlavaRa <ratdg@ya.ru>
2016-03-03 21:34:29 +0300

    Removes unused usings

--- External/Plugins/LoomContext/Projects/LoomPropertiesDialog.cs
+++ External/Plugins/LoomContext/Projects/LoomPropertiesDialog.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
-using ProjectManager.Projects.AS3;
-
 namespace LoomContext.Projects
 {
     public partial class LoomPropertiesDialog : ProjectManager.Controls.PropertiesDialog
