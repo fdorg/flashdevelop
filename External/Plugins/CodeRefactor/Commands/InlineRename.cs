fdorg/flashdevelop

commit b8f86b8237f8fa183727fdd5353dd31ec40bccba

Advanced Shortcut update 28
Resolved the bug in ToolStripMenuItemEx.Shortcut by doing all task using
reflection instead of using base.Shortcut.

PluginCore/PluginCore/Managers/ShortcutKeysManager.cs
@@ -12,10 +12,11 @@ namespace PluginCore.Managers
     /// </summary>
     public static class ShortcutKeysManager
     {
-        private static PropertyInfo p_Shortcuts;
-        private static PropertyInfo p_ToolStrips;
         private static PropertyInfo p_IsAssignedToDropDownItem;
+        private static PropertyInfo p_Properties;
+        private static PropertyInfo p_Shortcuts;
         private static MethodInfo m_GetToplevelOwnerToolStrip;
+        private static MethodInfo m_SetInteger;
 
         private static IList toolStrips;
 
@@ -27,11 +28,7 @@ namespace PluginCore.Managers
             {
                 if (toolStrips == null)
                 {
-                    if (p_ToolStrips == null)
-                    {
-                        p_ToolStrips = typeof(ToolStripManager).GetProperty("ToolStrips", BindingFlags.Static | BindingFlags.NonPublic);
-                    }
-                    toolStrips = (IList) p_ToolStrips.GetValue(null, null);
+                    toolStrips = ToolStripManager_ToolStrips();
                 }
                 return toolStrips;
             }
@@ -179,10 +176,11 @@ namespace PluginCore.Managers
 
         internal static bool ProcessShortcut(ref Message m, ShortcutKeys shortcut)
         {
-            if (!IsThreadUsingToolStrips() || !shortcut.IsExtended)
+            if (!IsThreadUsingToolStrips())
             {
                 return false;
             }
+
             var control = Control.FromChildHandle(m.HWnd);
             var parent = control;
             if (parent == null)
@@ -209,10 +207,10 @@ namespace PluginCore.Managers
 
             bool handled = false;
             bool prune = false;
-            int count = ToolStrips.Count;
+            int count = toolStrips.Count;
             for (int i = 0; i < count; i++)
             {
-                var strip = ToolStrips[i] as ToolStrip;
+                var strip = toolStrips[i] as ToolStrip;
                 bool flag = false;
                 bool isAssignedToDropDownItem = false;
                 if (strip == null)
@@ -220,54 +218,58 @@ namespace PluginCore.Managers
                     prune = true;
                     continue;
                 }
-                if (strip != control.ContextMenuStrip && strip.Shortcuts().Contains(shortcut))
+                if (strip != control.ContextMenuStrip)
                 {
-                    if (strip.IsDropDown)
+                    var strip_Shortcuts = strip.Shortcuts();
+                    if (strip_Shortcuts.Contains(shortcut))
                     {
-                        var down = strip as ToolStripDropDown;
-                        var firstDropDown = down.GetFirstDropDown() as ContextMenuStrip;
-                        if (firstDropDown != null)
+                        if (strip.IsDropDown)
                         {
-                            isAssignedToDropDownItem = firstDropDown.IsAssignedToDropDownItem();
-                            if (!isAssignedToDropDownItem)
+                            var down = strip as ToolStripDropDown;
+                            var firstDropDown = down.GetFirstDropDown() as ContextMenuStrip;
+                            if (firstDropDown != null)
                             {
-                                if (firstDropDown != control.ContextMenuStrip)
+                                isAssignedToDropDownItem = firstDropDown.IsAssignedToDropDownItem();
+                                if (!isAssignedToDropDownItem)
                                 {
-                                    continue;
+                                    if (firstDropDown != control.ContextMenuStrip)
+                                    {
+                                        continue;
+                                    }
+                                    flag = true;
                                 }
-                                flag = true;
                             }
                         }
-                    }
-                    if (!flag)
-                    {
-                        var toplevelOwnerToolStrip = strip.GetToplevelOwnerToolStrip();
-                        if (toplevelOwnerToolStrip != null)
+                        if (!flag)
                         {
-                            var rootHWnd = GetRootHWnd(toplevelOwnerToolStrip);
-                            var controlRef = GetRootHWnd(control);
-                            flag = rootHWnd.Handle == controlRef.Handle;
-                            if (flag)
+                            var toplevelOwnerToolStrip = strip.GetToplevelOwnerToolStrip();
+                            if (toplevelOwnerToolStrip != null)
                             {
-                                var form = Control.FromHandle(controlRef.Handle) as Form;
-                                if (form != null && form.IsMdiContainer)
+                                var rootHWnd = WindowsFormsUtils_GetRootHWnd(toplevelOwnerToolStrip);
+                                var controlRef = WindowsFormsUtils_GetRootHWnd(control);
+                                flag = rootHWnd.Handle == controlRef.Handle;
+                                if (flag)
                                 {
-                                    var form2 = toplevelOwnerToolStrip.FindForm();
-                                    if (form2 != form && form2 != null)
+                                    var form = Control.FromHandle(controlRef.Handle) as Form;
+                                    if (form != null && form.IsMdiContainer)
                                     {
-                                        flag = form2 == form.ActiveMdiChild;
+                                        var form2 = toplevelOwnerToolStrip.FindForm();
+                                        if (form2 != form && form2 != null)
+                                        {
+                                            flag = form2 == form.ActiveMdiChild;
+                                        }
                                     }
                                 }
                             }
                         }
-                    }
-                    if (flag || isAssignedToDropDownItem)
-                    {
-                        var item = strip.Shortcuts()[shortcut] as ToolStripMenuItemEx;
-                        if (item != null && item.ProcessCmdKeyInternal(ref m, shortcut))
+                        if (flag || isAssignedToDropDownItem)
                         {
-                            handled = true;
-                            break;
+                            var item = strip_Shortcuts[shortcut] as ToolStripMenuItemEx;
+                            if (item != null && item.ProcessCmdKeyInternal(ref m, shortcut))
+                            {
+                                handled = true;
+                                break;
+                            }
                         }
                     }
                 }
@@ -281,7 +283,7 @@ namespace PluginCore.Managers
 
         internal static bool IsThreadUsingToolStrips()
         {
-            return ToolStrips != null && ToolStrips.Count > 0;
+            return ToolStrips != null && toolStrips.Count > 0;
         }
 
         internal static void PruneToolStripList()
@@ -302,34 +304,19 @@ namespace PluginCore.Managers
 
         #region Reflections
 
-        internal static Hashtable Shortcuts(this ToolStrip @this)
-        {
-            if (p_Shortcuts == null)
-            {
-                p_Shortcuts = typeof(ToolStrip).GetProperty("Shortcuts", BindingFlags.Instance | BindingFlags.NonPublic);
-            }
-            return (Hashtable) p_Shortcuts.GetValue(@this, null);
-        }
-
+        // Reflection: System.Windows.Forms.ToolStripDropDown.IsAssignedToDropDownItem
+        // Cache: p_IsAssignedToDropDownItem
         internal static bool IsAssignedToDropDownItem(this ToolStripDropDown @this)
         {
             if (p_IsAssignedToDropDownItem == null)
             {
-                p_IsAssignedToDropDownItem = typeof(ToolStripDropDown).GetProperty("IsAssignedToDropDownItem", BindingFlags.Instance | BindingFlags.NonPublic);
+                p_IsAssignedToDropDownItem = typeof(ToolStripDropDown).GetProperty("IsAssignedToDropDownItem", BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.NonPublic);
             }
             return (bool) p_IsAssignedToDropDownItem.GetValue(@this, null);
         }
 
-        internal static ToolStripDropDown GetFirstDropDown(this ToolStripDropDown @this)
-        {
-            var down = @this;
-            for (var down2 = down.OwnerToolStrip() as ToolStripDropDown; down2 != null; down2 = down.OwnerToolStrip() as ToolStripDropDown)
-            {
-                down = down2;
-            }
-            return down;
-        }
-
+        // Reflection: System.Windows.Forms.ToolStripDropDown.OwnerToolStrip
+        // Cache: N/A
         internal static ToolStrip OwnerToolStrip(this ToolStripDropDown @this)
         {
             var ownerItem = @this.OwnerItem;
@@ -349,22 +336,87 @@ namespace PluginCore.Managers
             return null;
         }
 
+        // Reflection: System.Windows.Forms.ToolStripItem.Properties : System.Windows.Forms.PropertyStore
+        // Cache: p_Properties
+        internal static object Properties(this ToolStripItem @this)
+        {
+            if (p_Properties == null)
+            {
+                p_Properties = typeof(ToolStripItem).GetProperty("Properties", BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.NonPublic);
+            }
+            return (object) p_Properties.GetValue(@this, null);
+        }
+
+        // Reflection: System.Windows.Forms.ToolStrip.Shortcuts
+        // Cache: p_Shortcuts
+        internal static Hashtable Shortcuts(this ToolStrip @this)
+        {
+            if (p_Shortcuts == null)
+            {
+                p_Shortcuts = typeof(ToolStrip).GetProperty("Shortcuts", BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.NonPublic);
+            }
+            return (Hashtable) p_Shortcuts.GetValue(@this, null);
+        }
+
+        // Reflection: System.Windows.Forms.ToolStripManager.ToolStrips : System.Windows.Forms.ClientUtils+WeakRefCollection
+        // Cache: --
+        internal static IList ToolStripManager_ToolStrips()
+        {
+            return (IList) typeof(ToolStripManager).InvokeMember("ToolStrips", BindingFlags.DeclaredOnly | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetProperty, null, null, null);
+        }
+
+        // Reflection: System.Windows.Forms.ToolStripMenuItem.PropShortcutKeys
+        // Cache: --
+        internal static int ToolStripMenuItem_PropShortcutKeys()
+        {
+            return (int) typeof(ToolStripMenuItem).InvokeMember("PropShortcutKeys", BindingFlags.DeclaredOnly | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField, null, null, null);
+        }
+
+        // Reflection: System.Windows.Forms.ToolStripDropDown.GetFirstDropDown()
+        // Cache: N/A
+        internal static ToolStripDropDown GetFirstDropDown(this ToolStripDropDown @this)
+        {
+            var down = @this;
+            for (var down2 = down.OwnerToolStrip() as ToolStripDropDown; down2 != null; down2 = down.OwnerToolStrip() as ToolStripDropDown)
+            {
+                down = down2;
+            }
+            return down;
+        }
+
+        // Reflection: System.Windows.Forms.ToolStrip.GetToplevelOwnerToolStrip()
+        // Cache: m_GetToplevelOwnerToolStrip
         internal static ToolStrip GetToplevelOwnerToolStrip(this ToolStrip @this)
         {
             if (m_GetToplevelOwnerToolStrip == null)
             {
-                m_GetToplevelOwnerToolStrip = typeof(ToolStrip).GetMethod("GetToplevelOwnerToolStrip", BindingFlags.Instance | BindingFlags.NonPublic);
+                m_GetToplevelOwnerToolStrip = typeof(ToolStrip).GetMethod("GetToplevelOwnerToolStrip", BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.NonPublic);
             }
             return (ToolStrip) m_GetToplevelOwnerToolStrip.Invoke(@this, null);
         }
 
-        internal static HandleRef GetRootHWnd(Control control)
+        // Reflection: System.Windows.Forms.PropertyStore.SetInteger(Int32, Int32)
+        // Cache: m_SetInteger
+        internal static void Properties_SetInteger(this ToolStripMenuItemEx @this, int key, int value)
         {
-            return new HandleRef(control, GetAncestor(new HandleRef(new HandleRef(control, control.Handle), control.Handle), 2));
+            if (m_SetInteger == null)
+            {
+                m_SetInteger = @this.Properties.GetType().GetMethod("SetInteger", BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public);
+            }
+            m_SetInteger.Invoke(@this.Properties, new object[] { key, value });
         }
 
+        // Reflection: System.Windows.Forms.UnsafeNativeMethods.GetAncestor(HandleRef, Int32)
+        // Cache: N/A
         [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
-        internal static extern IntPtr GetAncestor(HandleRef hWnd, int flags);
+        internal static extern IntPtr UnsafeNativeMethods_GetAncestor(HandleRef hWnd, int flags);
+
+        // Reflection: System.Windows.Forms.WindowsFormsUtils.GetRootHWnd(Control), [inline] System.Windows.Forms.WindowsFormsUtils.GetRootHWnd(HandleRef)
+        // Cache: N/A
+        internal static HandleRef WindowsFormsUtils_GetRootHWnd(Control control)
+        {
+            return new HandleRef(control, UnsafeNativeMethods_GetAncestor(new HandleRef(new HandleRef(control, control.Handle), control.Handle), 2));
+        }
 
         #endregion
     }

commit bfb9f74598da5c34d28d9e52505d0ef67fca1c5a

Advanced Shortcut update 34
Updated according to the PR #952.
Fixed a bug caused in b8f86b8.

External/Plugins/CodeRefactor/Commands/InlineRename.cs
@@ -6,6 +6,7 @@ using CodeRefactor.Provider;
 using PluginCore;
 using PluginCore.Controls;
 using PluginCore.FRService;
+using PluginCore.Managers;
 using ScintillaNet;
 using ScintillaNet.Enums;
 using Keys = System.Windows.Forms.Keys;
@@ -59,6 +60,7 @@ namespace CodeRefactor.Commands
         private ReferenceInfo[] refs;
 
         private DelayedExecution delayedExecution;
+        private ShortcutKeys currentKeys;
         private List<string> history;
         private int historyIndex;
 
@@ -140,6 +142,7 @@ namespace CodeRefactor.Commands
             end = position;
             currentDoc = PluginBase.MainForm.CurrentDocument;
             delayedExecution = new DelayedExecution();
+            currentKeys = ShortcutKeys.None;
             history = new List<string>() { oldName };
             historyIndex = 0;
             this.includeComments = includeComments ?? false;
@@ -723,50 +726,48 @@ namespace CodeRefactor.Commands
                 case 0x0104: //WM_SYSKEYDOWN
                     var key = (Keys) (int) m.WParam;
                     var modifier = Control.ModifierKeys;
-                    switch (key)
+                    if (!ShortcutKeysManager.IsValidExtendedShortcutFirst(currentKeys.First))
                     {
-                        case Keys.Escape:
-                            OnCancel();
-                            return true;
-                        case Keys.Enter:
-                            OnApply();
-                            return true;
-                        case Keys.Back:
-                            if (CanBackspace) break;
-                            return true;
-                        case Keys.Left:
-                            if (!AtLeftmost) break;
-                            return true;
-                        case Keys.Delete:
-                            if (CanDelete) break;
-                            return true;
-                        case Keys.Right:
-                            if (!AtRightmost) break;
-                            if (sci.SelTextSize != 0) sci.SetSel(end, end);
-                            return true;
-                        case Keys.PageUp:
-                        case Keys.PageDown:
-                        case Keys.Up:
-                        case Keys.Down:
-                            if (!CanWrite) break;
-                            return true;
-                        case Keys.End:
-                            if (!CanWrite) break;
-                            sci.SetSel((modifier & Keys.Shift) == 0 ? end : sci.SelectionStart, end);
-                            return true;
-                        case Keys.Home:
-                            if (!CanWrite) break;
-                            sci.SetSel((modifier & Keys.Shift) == 0 ? start : sci.SelectionEnd, start);
-                            return true;
-                        case Keys.Tab:
-                            return true;
-                        default:
-                            if (ToolStripManager.IsValidShortcut(key | modifier))
-                            {
-                                return HandleShortcuts(key | modifier);
-                            }
-                            break;
+                        switch (key)
+                        {
+                            case Keys.Escape:
+                                OnCancel();
+                                return true;
+                            case Keys.Enter:
+                                OnApply();
+                                return true;
+                            case Keys.Back:
+                                if (CanBackspace) break;
+                                return true;
+                            case Keys.Left:
+                                if (!AtLeftmost) break;
+                                return true;
+                            case Keys.Delete:
+                                if (CanDelete) break;
+                                return true;
+                            case Keys.Right:
+                                if (!AtRightmost) break;
+                                if (sci.SelTextSize != 0) sci.SetSel(end, end);
+                                return true;
+                            case Keys.PageUp:
+                            case Keys.PageDown:
+                            case Keys.Up:
+                            case Keys.Down:
+                                if (!CanWrite) break;
+                                return true;
+                            case Keys.End:
+                                if (!CanWrite) break;
+                                sci.SetSel((modifier & Keys.Shift) == 0 ? end : sci.SelectionStart, end);
+                                return true;
+                            case Keys.Home:
+                                if (!CanWrite) break;
+                                sci.SetSel((modifier & Keys.Shift) == 0 ? start : sci.SelectionEnd, start);
+                                return true;
+                            case Keys.Tab:
+                                return true;
+                        }
                     }
+                    if (HandleShortcuts(key | modifier)) return true;
                     break;
 
                 case 0x0102: //WM_CHAR
@@ -804,34 +805,30 @@ namespace CodeRefactor.Commands
         /// </returns>
         private bool HandleShortcuts(Keys keys)
         {
-            switch (PluginBase.MainForm.GetShortcutId(keys))
+            string shortcutId;
+            if (PluginBase.MainForm.HandleShortcutManually(ref currentKeys, keys, out shortcutId))
             {
-                case "EditMenu.Paste":
-                    PerformPaste();
-                    break;
-                case "EditMenu.Redo":
-                    PerformRedo();
-                    break;
-                case "EditMenu.SelectAll":
-                    PerformSelectAll();
-                    break;
-                case "EditMenu.Undo":
-                    PerformUndo();
-                    break;
-                case "EditMenu.Copy":
-                case "EditMenu.Cut":
-                case "EditMenu.ToLowercase":
-                case "EditMenu.ToUppercase":
-                case "Scintilla.ResetZoom":
-                case "Scintilla.ZoomIn":
-                case "Scintilla.ZoomOut":
-                    return false;
-                default:
-                    //string.Format("Shortcut \"{0}\" cannot be used during renaming", shortcut.Key)
-                    break;
+                switch (shortcutId)
+                {
+                    case "EditMenu.Paste":       PerformPaste(); break;
+                    case "EditMenu.Redo":        PerformRedo(); break;
+                    case "EditMenu.SelectAll":   PerformSelectAll(); break;
+                    case "EditMenu.Undo":        PerformUndo(); break;
+                    case "EditMenu.Copy":        sci.Copy(); break;
+                    case "EditMenu.Cut":         if (sci.SelTextSize > 0) sci.Cut(); break;
+                    case "EditMenu.ToLowercase": sci.UpperCase(); break;
+                    case "EditMenu.ToUppercase": sci.LowerCase(); break;
+                    case "Scintilla.ResetZoom":  sci.ResetZoom(); break;
+                    case "Scintilla.ZoomIn":     sci.ZoomIn(); break;
+                    case "Scintilla.ZoomOut":    sci.ZoomOut(); break;
+                    default:
+                        //PluginBase.MainForm.StatusLabelText = string.Format("Shortcut \"{0}\" cannot be used during renaming.", shortcutId);
+                        break;
+                }
+                currentKeys = ShortcutKeys.None;
+                return true;
             }
-
-            return true;
+            return !currentKeys.IsNone && (ShortcutKeysManager.IsValidExtendedShortcutFirst(currentKeys.First) || ShortcutKeysManager.IsValidSimpleShortcutExclDeleteInsert(currentKeys.First));
         }
 
         #endregion

PluginCore/PluginCore/Managers/ShortcutKeysManager.cs
@@ -437,7 +437,7 @@ namespace PluginCore.Managers
 
         // Reflection: System.Windows.Forms.UnsafeNativeMethods.GetAncestor(HandleRef, Int32)
         // Cache: N/A
-        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        [DllImport("user32.dll", CharSet = CharSet.Auto, EntryPoint = "GetAncestor", ExactSpelling = true)]
         internal static extern IntPtr UnsafeNativeMethods_GetAncestor(HandleRef hWnd, int flags);
 
         // Reflection: System.Windows.Forms.WindowsFormsUtils.GetRootHWnd(Control), [inline] System.Windows.Forms.WindowsFormsUtils.GetRootHWnd(HandleRef)