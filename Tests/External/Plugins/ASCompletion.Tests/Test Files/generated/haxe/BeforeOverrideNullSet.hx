9a7d8a9cf3beb22e7693a770bd7a1775fec6572a
SlavaRa <ratdg@ya.ru>
2016-09-08 18:52:45 +0300

    Adds tests

---
+++ Tests/External/Plugins/ASCompletion.Tests/Test Files/generated/haxe/BeforeOverrideNullSet.hx
@@ -0,0 +1,13 @@
+﻿package;
+class Foo {
+	public function new() {}
+	public var foo(null, set):Int;
+	function set_foo(value:Int):Int {
+		return foo = value;
+	}
+}
+
+
+class Bar extends Foo {
+	override $(EntryPoint)foo
+}
