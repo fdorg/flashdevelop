1a1eb9c6aa2e156a4db4e01c292bd15a88d341b3
Shenjoku <shenjoku@gmail.com>
2016-10-07 19:08:42 -0700

    Undo bookmark change and fixed whitespace

--- FlashDevelop/Docking/TabbedDocument.cs
+++ FlashDevelop/Docking/TabbedDocument.cs
@@ -364,8 +364,8 @@ namespace FlashDevelop.Docking
             if (!Globals.MainForm.ClosingEntirely && File.Exists(this.FileName))
             {
                 FileInfo fi = new FileInfo(this.FileName);
-				if (this.fileInfo.IsReadOnly != fi.IsReadOnly) return true;
-				if (this.fileInfo.LastWriteTime != fi.LastWriteTime) return true;
+                if (this.fileInfo.IsReadOnly != fi.IsReadOnly) return true;
+                if (this.fileInfo.LastWriteTime != fi.LastWriteTime) return true;
             }
             return false;
         }
@@ -468,17 +468,10 @@ namespace FlashDevelop.Docking
                 this.SciControl.IsReadOnly = FileHelper.FileIsReadOnly(this.FileName);
                 this.SciControl.SetSel(position, position);
                 this.SciControl.EmptyUndoBuffer();
+                this.InitBookmarks();
 
-				List<Int32> bookmarksCopy = this.bookmarks;
-				foreach (var lineNum in bookmarksCopy)
-				{
-					MarkerManager.ToggleMarker(SciControl, 0, lineNum);
-				}
-
-				this.InitBookmarks();
-
-				this.fileInfo = new FileInfo(this.FileName);
-			}
+                this.fileInfo = new FileInfo(this.FileName);
+            }
             Globals.MainForm.OnDocumentReload(this);
         }
 
