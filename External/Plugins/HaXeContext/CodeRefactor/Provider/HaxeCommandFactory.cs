6419c30d88fd08992537b764fa8f90f73889a95b
Slava Buynov <SlavaB@playtika.com>
2017-01-26 15:00:09 +0300

    Small cleanup...

--- External/Plugins/HaXeContext/CodeRefactor/Provider/HaxeCommandFactory.cs
+++ External/Plugins/HaXeContext/CodeRefactor/Provider/HaxeCommandFactory.cs
@@ -5,6 +5,7 @@ using ASCompletion.Model;
 using CodeRefactor.Commands;
 using CodeRefactor.Provider;
 using PluginCore.FRService;
+using PluginCore.Utilities;
 
 namespace HaXeContext.CodeRefactor.Provider
 {
@@ -16,8 +17,9 @@ namespace HaXeContext.CodeRefactor.Provider
         {
             if (target.Member != null && (target.Member.Flags & FlagType.LocalVar) != 0)
             {
-                var settings = (HaXeSettings) ASContext.GetLanguageContext("haxe").Settings;
-                if (settings.CompletionMode != HaxeCompletionModeEnum.FlashDevelop)
+                var context = (HaXeContext.Context) ASContext.GetLanguageContext("haxe");
+                if (((HaXeSettings) context.Settings).CompletionMode != HaxeCompletionModeEnum.FlashDevelop
+                    && context.GetCurrentSDKVersion().IsGreaterThanOrEquals(new SemVer("3.2.0")))
                 {
                     return new Commands.HaxeFindAllReferences(target, output, ignoreDeclarations)
                     {
