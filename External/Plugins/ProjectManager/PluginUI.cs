f97dd83d52064f8fbd17024b074c140936daa84e
Slava Buynov <SlavaB@playtika.com>
2017-02-18 19:03:07 +0300

    Import a Haxe hxml project

--- External/Plugins/ProjectManager/PluginUI.cs
+++ External/Plugins/ProjectManager/PluginUI.cs
@@ -71,22 +71,24 @@ namespace ProjectManager
 
             help = new LinkLabel();
             string[] helpParts = String.Format(TextHelper.GetString("Info.NoProjectsOpenLink"), "\n").Split('|');
-            string[] helpActions = { "create", "open", "import" };
+            string[] helpActions = { "create", "open", "import", "import" };
+            var helpActionsLength = helpActions.Length;
+            int[] linkStart = new int[helpActionsLength];
+            int[] linkLength = new int[helpActionsLength];
             string helpText = "";
-            int[] linkStart = { 0, 0, 0 };
-            int[] linkLength = { 0, 0, 0 };
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < helpActionsLength; i++)
             {
-                if (helpParts.Length > i * 2)
+                var linkIndex = i * 2;
+                if (helpParts.Length > linkIndex)
                 {
-                    helpText += helpParts[i * 2];
+                    helpText += helpParts[linkIndex];
                     linkStart[i] = helpText.Length;
-                    helpText += helpParts[i * 2 + 1];
-                    linkLength[i] = helpParts[i * 2 + 1].Length;
+                    helpText += helpParts[linkIndex + 1];
+                    linkLength[i] = helpParts[linkIndex + 1].Length;
                 }
             }
             help.Text = helpText + helpParts[helpParts.Length - 1];
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < helpActionsLength; i++)
             {
                 help.Links.Add(linkStart[i], linkLength[i], helpActions[i]);
             }
