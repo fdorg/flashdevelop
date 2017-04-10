0a81685d33b18e6a8392a1586e9d8257d5317187
Slava Buynov <SlavaB@playtika.com>
2017-04-10 18:07:17 +0300

    PVS-Studio headers

--- External/Plugins/CodeRefactor/BatchProcessors/FormatCodeProcessor.cs
+++ External/Plugins/CodeRefactor/BatchProcessors/FormatCodeProcessor.cs
@@ -1,4 +1,6 @@
-﻿using CodeRefactor.Managers;
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+using CodeRefactor.Managers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
