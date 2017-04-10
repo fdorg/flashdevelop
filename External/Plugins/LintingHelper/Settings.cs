1d2a1a647523612317b0f69002c62a5615e7f932
chipshort <christophotter@gmail.com>
2017-04-10 17:37:23 +0200

    Add LintingManager (#1540)

---
+++ External/Plugins/LintingHelper/Settings.cs
@@ -0,0 +1,32 @@
+﻿using PluginCore.Localization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace LintingHelper
+{
+    [Serializable]
+    public class Settings
+    {
+        private bool lintOnSave = true;
+        private bool lintOnOpen = true;
+
+        [DisplayName("Lint after saving a file")]
+        [LocalizedDescription("LintingHelper.Description.LintOnSave"), DefaultValue(true)]
+        public bool LintOnSave
+        {
+            get { return this.lintOnSave; }
+            set { this.lintOnSave = value; }
+        }
+
+        [DisplayName("Lint after opening a file")]
+        [LocalizedDescription("LintingHelper.Description.LintOnOpen"), DefaultValue(true)]
+        public bool LintOnOpen
+        {
+            get { return this.lintOnOpen; }
+            set { this.lintOnOpen = value; }
+        }
+    }
+}
