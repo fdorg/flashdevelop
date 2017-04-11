29c930ce634f240f4be63153ce169ddb53a4c22f
Christoph Otter <christophotter@gmail.com>
2017-04-11 12:46:36 +0200

    check for compiler version in DiagnosticsLinter

--- External/Plugins/HaXeContext/Linters/DiagnosticsLinter.cs
+++ External/Plugins/HaXeContext/Linters/DiagnosticsLinter.cs
@@ -7,6 +7,7 @@ using ASCompletion.Completion;
 using ASCompletion.Context;
 using PluginCore;
 using PluginCore.Managers;
+using PluginCore.Utilities;
 
 namespace HaXeContext.Linters
 {
@@ -33,44 +34,51 @@ namespace HaXeContext.Linters
                 var exprStub = new ASExpr();
                 exprStub.Position = 0;
 
-                var hc = context.GetHaxeComplete(document.SciControl, exprStub, true, HaxeCompilerService.DIAGNOSTICS);
-                hc.GetDiagnostics((complete, results, status) =>
+                var completionMode = ((HaXeSettings) context.Settings).CompletionMode;
+                var haxeVersion = context.GetCurrentSDKVersion();
+                if (completionMode != HaxeCompletionModeEnum.FlashDevelop && haxeVersion.IsGreaterThanOrEquals(new SemVer("3.3.0")))
                 {
-                    var list = new List<LintingResult>();
-                    if (results == null)
+                    var hc = context.GetHaxeComplete(document.SciControl, exprStub, true, HaxeCompilerService.DIAGNOSTICS);
+                    hc.GetDiagnostics((complete, results, status) =>
                     {
-                        callback(null);
-                        return;
-                    }
-
-                    foreach (var res in results)
-                    {
-                        var line = res.Range.LineStart + 1;
-                        var firstChar = document.SciControl.PositionFromLine(line) + res.Range.CharacterStart;
-                        var lastChar = document.SciControl.PositionFromLine(res.Range.LineEnd + 1) + res.Range.CharacterEnd;
-                        var result = new LintingResult
+                        var list = new List<LintingResult>();
+                        if (results == null)
                         {
-                            File = res.Range.Path,
-                            FirstChar = res.Range.CharacterStart,
-                            Length = lastChar - firstChar,
-                            Line = line,
-                            Severity = conversionDict[res.Severity]
-                        };
-
-                        switch (res.Kind)
-                        {
-                            case HaxeDiagnosticsKind.UNUSEDIMPORT:
-                                result.Description = "Unused import";
-                                break;
-                            case HaxeDiagnosticsKind.UNUSEDVAR:
-                                result.Description = "Unused variable";
-                                break;
+                            callback(null);
+                            return;
                         }
 
-                        list.Add(result);
-                    }
-                    callback(list);
-                });
+                        foreach (var res in results)
+                        {
+                            var line = res.Range.LineStart + 1;
+                            var firstChar = document.SciControl.PositionFromLine(line) + res.Range.CharacterStart;
+                            var lastChar = document.SciControl.PositionFromLine(res.Range.LineEnd + 1) + res.Range.CharacterEnd;
+                            var result = new LintingResult
+                            {
+                                File = res.Range.Path,
+                                FirstChar = res.Range.CharacterStart,
+                                Length = lastChar - firstChar,
+                                Line = line,
+                                Severity = conversionDict[res.Severity]
+                            };
+
+                            switch (res.Kind)
+                            {
+                                case HaxeDiagnosticsKind.UNUSEDIMPORT:
+                                    result.Description = "Unused import";
+                                    break;
+                                case HaxeDiagnosticsKind.UNUSEDVAR:
+                                    result.Description = "Unused variable";
+                                    break;
+                            }
+
+                            list.Add(result);
+                        }
+                        callback(list);
+                    });
+                }
+
+                    
             }
         }
     }
