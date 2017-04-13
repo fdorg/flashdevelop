45cba99c4d9fdaaf623eedf74d5b5f31443eb3bc
SlavaRa <ratdg@ya.ru>
2017-04-13 14:18:38 +0300

    Fixed NRE in DiagnosticsLinter (#1558)

--- External/Plugins/HaXeContext/Linters/DiagnosticsLinter.cs
+++ External/Plugins/HaXeContext/Linters/DiagnosticsLinter.cs
@@ -1,8 +1,5 @@
 ﻿using LintingHelper;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using ASCompletion.Completion;
 using ASCompletion.Context;
 using PluginCore;
@@ -14,8 +11,8 @@ namespace HaXeContext.Linters
 {
     class DiagnosticsLinter : ILintProvider
     {
-        private static Dictionary<HaxeDiagnosticsSeverity, LintingSeverity> conversionDict =
-            new Dictionary<HaxeDiagnosticsSeverity, LintingSeverity>()
+        static readonly Dictionary<HaxeDiagnosticsSeverity, LintingSeverity> DiagnosticsSeverityToLintingSeverity =
+            new Dictionary<HaxeDiagnosticsSeverity, LintingSeverity>
             {
                 {HaxeDiagnosticsSeverity.ERROR, LintingSeverity.Error},
                 {HaxeDiagnosticsSeverity.INFO, LintingSeverity.Info},
@@ -24,62 +21,53 @@ namespace HaXeContext.Linters
 
         public void LintAsync(string[] files, LintCallback callback)
         {
+            var context = ASContext.GetLanguageContext("haxe") as Context;
+            if (context == null) return;
+            var completionMode = ((HaXeSettings) context.Settings).CompletionMode;
+            var haxeVersion = context.GetCurrentSDKVersion();
+            if (completionMode == HaxeCompletionModeEnum.FlashDevelop || haxeVersion.IsOlderThan(new SemVer("3.3.0"))) return;
             foreach (var file in files)
             {
                 var document = DocumentManager.FindDocument(file) ?? PluginBase.MainForm.OpenEditableDocument(file) as ITabbedDocument;
-                var context = ASContext.GetLanguageContext("haxe") as Context;
-
-                if (document == null || context == null) continue;
-
-                //stub expression is needed for position
-                var exprStub = new ASExpr();
-                exprStub.Position = 0;
-
-                var completionMode = ((HaXeSettings) context.Settings).CompletionMode;
-                var haxeVersion = context.GetCurrentSDKVersion();
-                if (completionMode != HaxeCompletionModeEnum.FlashDevelop && haxeVersion.IsGreaterThanOrEquals(new SemVer("3.3.0")))
+                if (document == null) continue;
+                var hc = context.GetHaxeComplete(document.SciControl, new ASExpr {Position = 0}, true, HaxeCompilerService.DIAGNOSTICS);
+                hc.GetDiagnostics((complete, results, status) =>
                 {
-                    var hc = context.GetHaxeComplete(document.SciControl, exprStub, true, HaxeCompilerService.DIAGNOSTICS);
-                    hc.GetDiagnostics((complete, results, status) =>
+                    var sci = document.SciControl;
+                    if (results == null || sci == null)
                     {
-                        var list = new List<LintingResult>();
-                        if (results == null)
+                        callback(null);
+                        return;
+                    }
+                    var list = new List<LintingResult>();
+                    foreach (var res in results)
+                    {
+                        var line = res.Range.LineStart + 1;
+                        var firstChar = sci.PositionFromLine(line) + res.Range.CharacterStart;
+                        var lastChar = sci.PositionFromLine(res.Range.LineEnd + 1) + res.Range.CharacterEnd;
+                        var result = new LintingResult
                         {
-                            callback(null);
-                            return;
+                            File = res.Range.Path,
+                            FirstChar = res.Range.CharacterStart,
+                            Length = lastChar - firstChar,
+                            Line = line,
+                            Severity = DiagnosticsSeverityToLintingSeverity[res.Severity]
+                        };
+
+                        switch (res.Kind)
+                        {
+                            case HaxeDiagnosticsKind.UNUSEDIMPORT:
+                                result.Description = TextHelper.GetString("HaXeContext.Info.UnusedImport");
+                                break;
+                            case HaxeDiagnosticsKind.UNUSEDVAR:
+                                result.Description = TextHelper.GetString("HaXeContext.Info.UnusedVariable");
+                                break;
                         }
 
-                        foreach (var res in results)
-                        {
-                            var line = res.Range.LineStart + 1;
-                            var firstChar = document.SciControl.PositionFromLine(line) + res.Range.CharacterStart;
-                            var lastChar = document.SciControl.PositionFromLine(res.Range.LineEnd + 1) + res.Range.CharacterEnd;
-                            var result = new LintingResult
-                            {
-                                File = res.Range.Path,
-                                FirstChar = res.Range.CharacterStart,
-                                Length = lastChar - firstChar,
-                                Line = line,
-                                Severity = conversionDict[res.Severity]
-                            };
-
-                            switch (res.Kind)
-                            {
-                                case HaxeDiagnosticsKind.UNUSEDIMPORT:
-                                    result.Description = TextHelper.GetString("HaXeContext.Info.UnusedImport");
-                                    break;
-                                case HaxeDiagnosticsKind.UNUSEDVAR:
-                                    result.Description = TextHelper.GetString("HaXeContext.Info.UnusedVariable");
-                                    break;
-                            }
-
-                            list.Add(result);
-                        }
-                        callback(list);
-                    });
-                }
-
-                    
+                        list.Add(result);
+                    }
+                    callback(list);
+                });
             }
         }
     }
