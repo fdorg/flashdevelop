f78974d679b6dae134f50d4a3dc881b6f7569e1e
SlavaRa <ratdg@ya.ru>
2017-04-15 01:03:04 +0300

    [Haxe][ASGenerator] Improved property override (#1567)

---
+++ Tests/External/Plugins/ASCompletion.Tests/Test Files/generated/haxe/AfterOverrideGetSet_2.hx
@@ -0,0 +1,17 @@
+﻿package;
+class Foo {
+	public function new() {}
+	@:isVar public var foo(get, set):Int;
+	function get_foo():Int return 1;
+	function set_foo(value:Int) return foo = value;
+}
+
+
+class Bar extends Foo {
+	//@:setter(foo)
+	override function set_foo(value:Int):Int {
+		return super.set_foo(value);
+	}
+	
+	override function get_foo():Int return 2;
+}
