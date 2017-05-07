85f145e4b4ade8cf64913961d0a414f7beb48da4
Slava Buynov <SlavaB@playtika.com>
2017-05-07 10:33:47 +0300

    Cleanup...

--- External/Plugins/ASCompletion/Completion/ASComplete.cs
+++ External/Plugins/ASCompletion/Completion/ASComplete.cs
@@ -3513,8 +3513,7 @@ namespace ASCompletion.Completion
                             if (haXe && testWord == "cast") expression.SubExpressions.Add(testWord);
                             expression.SubExpressions.Add(sbSub.ToString());
                             sb.Insert(0, ".#" + (subCount++) + "~"); // method call or sub expression
-                            if (testWord == "return" || testWord == "case" || testWord == "default" ||
-                                (haXe && testWord == "cast"))
+                            if (testWord == "return" || testWord == "case" || testWord == "default" || (haXe && testWord == "cast"))
                             {
                                 // AS3, AS2, Loom ex: return (a as B).<complete>
                                 // Haxe ex: return cast(a, B).<complete>
