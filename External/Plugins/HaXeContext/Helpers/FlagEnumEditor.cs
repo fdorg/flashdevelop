0000be1b421fe896fd30ebdbe0832ac672eb722e
Christoph Otter <christophotter@gmail.com>
2017-08-22 10:51:41 +0200

    Add editor for CompletionFeatures setting

--- External/Plugins/HaXeContext/Helpers/FlagEnumEditor.cs
+++ External/Plugins/HaXeContext/Helpers/FlagEnumEditor.cs
@@ -11,39 +11,94 @@ namespace HaXeContext.Helpers
 {
     class FlagEnumEditor : UITypeEditor
     {
-        CheckedList
+        FlagCheckedListBox<CompletionFeatures> list;
 
         public FlagEnumEditor()
         {
-            
+            list = new FlagCheckedListBox<CompletionFeatures>();
+        }
+
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.DropDown;
         }
 
         public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
         {
-            if (context == null || context.PropertyDescriptor == null) return null;
+            if (context?.PropertyDescriptor == null) return null;
 
             var service = (IWindowsFormsEditorService) provider.GetService(typeof(IWindowsFormsEditorService));
-            
-            service.DropDownControl();
-            
-            return base.EditValue(context, provider, value);
+
+            list.Value = EnumHelper.ConvertToInt(value);
+            service.DropDownControl(list);
+
+            return list.Value;
+
+            //return base.EditValue(context, provider, value);
+        }
+    }
+
+    class CheckedListBoxItem<T>
+    {
+        public int IntValue;
+        public T Value;
+
+        public override string ToString()
+        {
+            return Enum.GetName(typeof(T), Value);
         }
     }
 
     class FlagCheckedListBox<T> : CheckedListBox
     {
-        int internalValue = 0;
+        public FlagCheckedListBox()
+        {
+            CheckOnClick = true;
+            foreach (T val in Enum.GetValues(typeof(T)))
+            {
+                var pos = EnumHelper.ConvertToInt(val);
 
-        public T Value
+                var item = new CheckedListBoxItem<T>
+                {
+                    Value = val,
+                    IntValue = pos
+                };
+                Items.Add(item, false);
+            }
+        }
+
+        public int Value
         {
             get
             {
-                return Convert.ChangeType(internalValue, typeof(Enum));
+                var value = 0;
+                foreach (CheckedListBoxItem<T> item in CheckedItems)
+                {
+                    value |= item.IntValue;
+                }
+                return value;
             }
             set
             {
-                
+                for (var i = 0; i < Items.Count; ++i)
+                {
+                    var item = (CheckedListBoxItem<T>) Items[i];
+                    SetItemChecked(i, (item.IntValue & value) == item.IntValue);
+                }
             }
         }
+    }
+
+    class EnumHelper
+    {
+        public static int ConvertToInt<T>(T value)
+        {
+            return (int)Convert.ChangeType(value, typeof(int));
+        }
+
+        //public static T ConvertToEnum<T>(int value)
+        //{
+        //    return (T)Convert.ChangeType(value, typeof(Enum));
+        //}
     }
 }
