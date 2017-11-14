3d7345de4cb186f5e94a402f5d9090b7fe718486
Slava Buynov <slavab@playtika.com>
2017-11-14 18:05:23 +0300

    [Haxe] Fixed "Organize Imports" removes implicit imports of types within modules. fixes #191

---
+++ External/Plugins/HaXeContext/CodeRefactor/Commands/OrganizeImports.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ASCompletion.Context;
+using ASCompletion.Model;
+
+namespace HaXeContext.CodeRefactor.Commands
+{
+    class OrganizeImports : global::CodeRefactor.Commands.OrganizeImports
+    {
+        protected override List<string> GetUniqueImports(List<MemberModel> imports, string searchInText, string sourceFile)
+        {
+            var result = new HashSet<string>();
+            foreach (var import in imports)
+            {
+                if (result.Contains(import.Type)) continue;
+                var inFile = import.InFile ?? ASContext.Context.ResolveType(import.Name, FileModel.Ignore).InFile;
+                if (inFile == null || inFile.Classes.Count == 1)
+                {
+                    if (MemberTypeImported(import.Name, searchInText, sourceFile)) result.Add(import.Type);
+                }
+                else if (inFile.Classes.Any(it => MemberTypeImported(it.Name, searchInText, sourceFile)))
+                {
+                    result.Add(import.Type);
+                }
+            }
+            return result.ToList();
+        }
+    }
+}
---
+++ Tests/External/Plugins/HaxeContext.Tests/Commands/RefactorCommandTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using CodeRefactor;
+using CodeRefactor.Provider;
+using HaXeContext.CodeRefactor.Provider;
+using HaXeContext.TestUtils;
+using NUnit.Framework;
+
+namespace HaXeContext.Commands
+{
+    [TestFixture]
+    class RefactorCommandTests : CodeRefactorTests
+    {
+        [TestFixture]
+        public class OrganizeImports : RefactorCommandTests
+        {
+            protected static string ReadAllTextHaxe(string fileName) => TestFile.ReadAllText($"{nameof(HaXeContext)}.Test_Files.coderefactor.organizeimports.{fileName}.hx");
+
+            [TestFixtureSetUp]
+            public void OrganizeImportsFixtureSetUp()
+            {
+                // Needed for preprocessor directives...
+                Sci.SetProperty("fold", "1");
+                Sci.SetProperty("fold.preprocessor", "1");
+                CommandFactoryProvider.Register("haxe", new HaxeCommandFactory());
+            }
+
+            public IEnumerable<TestCaseData> HaxeTestCases
+            {
+                get
+                {
+                    yield return
+                        new TestCaseData(ReadAllTextHaxe("BeforeOrganizeImports_issue191_1"), "BeforeOrganizeImports_issue191_1.hx")
+                            .Returns(ReadAllTextHaxe("AfterOrganizeImports_issue191_1"))
+                            .SetName("Issue191. Case 1.")
+                            .SetDescription("https://github.com/fdorg/flashdevelop/issues/191");
+                }
+            }
+
+            [Test, TestCaseSource(nameof(HaxeTestCases))]
+            public string Haxe(string sourceText, string fileName) => global::CodeRefactor.Commands.RefactorCommandTests.OrganizeImports.HaxeImpl(Sci, sourceText, fileName);
+        }
+    }
+}
