04e7eb0f2c265eca30492c54e803edbaac9fe2fe
Slava Buynov <slavab@playtika.com>
2017-11-30 17:04:05 +0300

    tests

---
+++ Tests/External/Plugins/HaxeContext.Tests/Test Files/coderefactor/organizeimports/BeforeOrganizeImports_withElseIfDirective.hx
@@ -0,0 +1,15 @@
+package org.fd;
+#if flash
+import flash.display.Sprite;
+import flixel.FlxGame;
+#elseif windows
+import cpp.abi.Abi;
+#end
+
+class Main
+{
+    public static function main():Void
+    {   
+
+    }
+}
