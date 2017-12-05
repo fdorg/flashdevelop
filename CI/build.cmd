b422ee8bdc870651b63224aef0100d72a56720fd
Evgeny Pavlovskiy <evgeny.pavlovskiy@doclerholding.com>
2017-12-05 09:49:45 +0100

    Fix CI/build.cmd verification of build/test errors, and introduce a compile error into a test to verify that works.

--- CI/build.cmd
+++ CI/build.cmd
@@ -8,14 +8,14 @@ set PATH=%PATH%;C:\Program Files\7-Zip\
 
 :flashdevelop
 
-:: Check for build errors
-if %errorlevel% neq 0 goto :error
-
 :: Extract version from HEAD
 call SetVersion.bat
 
 :: Build the main solution and run tests
 msbuild FlashDevelop.sln /p:Configuration=Release+Tests /p:Platform="x86" /t:Rebuild %MSBuildLogger%
+
+:: Check for build errors
+if %errorlevel% neq 0 goto :error
 
 if "%AppVeyorCI%" neq "" powershell.exe -file ci\tests.ps1
 
