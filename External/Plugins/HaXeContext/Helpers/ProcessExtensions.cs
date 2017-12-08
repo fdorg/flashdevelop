4ede4dcdadc3803617fadb2be4f09e1a3c682620
Neverbirth <neverbirth@gmail.com>
2017-12-08 22:37:30 +0100

    Missed file...

---
+++ External/Plugins/HaXeContext/Helpers/ProcessExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace HaXeContext.Helpers
+{
+    internal static class ProcessExtensions
+    {
+        public static ProcessStartInfo Clone(this ProcessStartInfo src)
+        {
+            return new ProcessStartInfo
+            {
+                Arguments = src.Arguments,
+                CreateNoWindow = src.CreateNoWindow,
+                Domain = src.Domain,
+                ErrorDialog = src.ErrorDialog,
+                ErrorDialogParentHandle = src.ErrorDialogParentHandle,
+                FileName = src.FileName,
+                LoadUserProfile = src.LoadUserProfile,
+                Password = src.Password,
+                RedirectStandardError = src.RedirectStandardError,
+                RedirectStandardInput = src.RedirectStandardInput,
+                RedirectStandardOutput = src.RedirectStandardOutput,
+                StandardErrorEncoding = src.StandardErrorEncoding,
+                StandardOutputEncoding = src.StandardOutputEncoding,
+                UserName = src.UserName,
+                UseShellExecute = src.UseShellExecute,
+                Verb = src.Verb,
+                WindowStyle = src.WindowStyle,
+                WorkingDirectory = src.WorkingDirectory
+            };
+        }
+    }
+}
