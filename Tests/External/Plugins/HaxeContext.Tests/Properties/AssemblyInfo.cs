b8d1e7d30aba1080b7f9f3a5b5ec3b15b0bc1b9b
Slava Buynov <slavab@playtika.com>
2018-01-03 06:21:05 +0300

    DistroConfig.DISTRIBUTION_COPYRIGHT = FlashDevelop.org 2005-2018

--- Tests/External/Plugins/HaxeContext.Tests/Properties/AssemblyInfo.cs
+++ Tests/External/Plugins/HaxeContext.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using PluginCore;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HaxeContext.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany(DistroConfig.DISTRIBUTION_COMPANY)]
 [assembly: AssemblyProduct("HaxeContext.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright(DistroConfig.DISTRIBUTION_COPYRIGHT)]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
