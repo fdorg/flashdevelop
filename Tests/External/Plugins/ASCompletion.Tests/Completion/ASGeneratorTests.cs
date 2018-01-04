c707e1df8c754ba900b59b34046f70b97439a7ec
Slava Buynov <slavab@playtika.com>
2018-01-04 14:49:04 +0300

    WIP

--- Tests/External/Plugins/ASCompletion.Tests/Completion/ASGeneratorTests.cs
+++ Tests/External/Plugins/ASCompletion.Tests/Completion/ASGeneratorTests.cs
@@ -1,6 +1,4 @@
-﻿// TODO: Tests with different formatting options using parameterized tests
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,91 +6,22 @@ using ASCompletion.Context;
 using ASCompletion.Model;
 using ASCompletion.Settings;
 using ASCompletion.TestUtils;
-using FlashDevelop;
 using NSubstitute;
 using NUnit.Framework;
 using PluginCore;
 using ScintillaNet;
-using ScintillaNet.Enums;
 using System.Text.RegularExpressions;
 using AS3Context;
 using HaXeContext;
 using PluginCore.Helpers;
 using PluginCore.Managers;
 
+// TODO: Tests with different formatting options using parameterized tests
+
 namespace ASCompletion.Completion
 {
-    [TestFixture]
-    public class ASGeneratorTests
+    public class ASGeneratorTests : ASCompletionTests
     {
-#pragma warning disable CS0436 // Type conflicts with imported type
-        private MainForm mainForm;
-#pragma warning restore CS0436 // Type conflicts with imported type
-        private ISettings settings;
-        private ITabbedDocument doc;
-
-        [TestFixtureSetUp]
-        public void FixtureSetUp()
-        {
-#pragma warning disable CS0436 // Type conflicts with imported type
-            mainForm = new MainForm();
-#pragma warning restore CS0436 // Type conflicts with imported type
-            settings = Substitute.For<ISettings>();
-            settings.UseTabs = true;
-            settings.IndentSize = 4;
-            settings.SmartIndentType = SmartIndent.CPP;
-            settings.TabIndents = true;
-            settings.TabWidth = 4;
-            doc = Substitute.For<ITabbedDocument>();
-            mainForm.Settings = settings;
-            mainForm.CurrentDocument = doc;
-            mainForm.StandaloneMode = true;
-            PluginBase.Initialize(mainForm);
-            FlashDevelop.Managers.ScintillaManager.LoadConfiguration();
-        }
-
-        [TestFixtureTearDown]
-        public void FixtureTearDown()
-        {
-            settings = null;
-            doc = null;
-            mainForm.Dispose();
-            mainForm = null;
-        }
-
-        private ScintillaControl GetBaseScintillaControl()
-        {
-            return new ScintillaControl
-            {
-                Encoding = System.Text.Encoding.UTF8,
-                CodePage = 65001,
-                Indent = settings.IndentSize,
-                Lexer = 3,
-                StyleBits = 7,
-                IsTabIndents = settings.TabIndents,
-                IsUseTabs = settings.UseTabs,
-                TabWidth = settings.TabWidth
-            };
-        }
-
-        private static void SetAs3Features(ScintillaControl sci)
-        {
-            if (sci.ConfigurationLanguage != "as3")
-            {
-                sci.ConfigurationLanguage = "as3";
-                ASContext.Context.SetAs3Features();
-            }
-        }
-
-        private static void SetHaxeFeatures(ScintillaControl sci)
-        {
-            if (sci.ConfigurationLanguage != "haxe")
-            {
-                sci.ConfigurationLanguage = "haxe";
-                ASContext.Context.SetHaxeFeatures();
-            }
-        }
-
         public class GetBodyStart : ASGeneratorTests
         {
             public IEnumerable<TestCaseData> GetBodyStartTestCases
@@ -121,7 +50,6 @@ namespace ASCompletion.Completion
             [Test, TestCaseSource(nameof(GetBodyStartTestCases))]
             public void Common(string text, int lineStart, int lineEnd, string resultText, int bodyStart)
             {
-                var sci = GetBaseScintillaControl();
                 sci.Text = text;
                 sci.ConfigurationLanguage = "haxe";
                 sci.Colourise(0, -1);
@@ -135,18 +63,6 @@ namespace ASCompletion.Completion
         [TestFixture]
         public class ContextualActions : ASGeneratorTests
         {
-            [TestFixtureSetUp]
-            public void ContextualActionsSetup()
-            {
-                var pluginMain = Substitute.For<PluginMain>();
-                var pluginUiMock = new PluginUIMock(pluginMain);
-                pluginMain.MenuItems.Returns(new List<System.Windows.Forms.ToolStripItem>());
-                pluginMain.Settings.Returns(new GeneralSettings());
-                pluginMain.Panel.Returns(pluginUiMock);
-                ASContext.GlobalInit(pluginMain);
-                ASContext.Context = Substitute.For<IASContext>();
-            }
-
             [TestFixture]
             class ShowEventsList : ContextualActions
             {
@@ -212,28 +128,11 @@ namespace ASCompletion.Completion
                     return dataEventModel;
                 }
             }
-
         }
 
         [TestFixture]
         public class GenerateJob : ASGeneratorTests
         {
-            protected ScintillaControl sci;
-
-            [TestFixtureSetUp]
-            public void GenerateJobSetup()
-            {
-                var pluginMain = Substitute.For<PluginMain>();
-                var pluginUiMock = new PluginUIMock(pluginMain);
-                pluginMain.MenuItems.Returns(new List<System.Windows.Forms.ToolStripItem>());
-                pluginMain.Settings.Returns(new GeneralSettings());
-                pluginMain.Panel.Returns(pluginUiMock);
-                ASContext.GlobalInit(pluginMain);
-                ASContext.Context = Substitute.For<IASContext>();
-
-                sci = GetBaseScintillaControl();
-                doc.SciControl.Returns(sci);
-            }
 
             [TestFixture]
             public class FieldFromParameter : GenerateJob
@@ -3481,29 +3380,5 @@ namespace ASCompletion.Completion
         protected static string ReadAllTextHaxe(string fileName) => TestFile.ReadAllText(GetFullPathHaxe(fileName));
 
         protected static string GetFullPathHaxe(string fileName) => $"ASCompletion.Test_Files.generated.haxe.{fileName}.hx";
-
-        protected static void SetSrc(ScintillaControl sci, string sourceText)
-        {
-            sci.Text = sourceText;
-            SnippetHelper.PostProcessSnippets(sci, 0);
-            var currentModel = ASContext.Context.CurrentModel;
-            new ASFileParser().ParseSrc(currentModel, sci.Text);
-            var line = sci.CurrentLine;
-            var currentClass = currentModel.Classes.FirstOrDefault(line);
-            ASContext.Context.CurrentClass.Returns(currentClass);
-            var currentMember = currentClass.Members.FirstOrDefault(line);
-            ASContext.Context.CurrentMember.Returns(currentMember);
-            ASGenerator.contextToken = sci.GetWordFromPosition(sci.CurrentPos);
-        }
-    }
-}
-
-public static class CollectionExtensions
-{
-    public static MemberModel FirstOrDefault(this MemberList list, int line) => list.Items.FirstOrDefault(line);
-
-    public static TSource FirstOrDefault<TSource>(this ICollection<TSource> items, int line) where TSource : MemberModel
-    {
-        return items.FirstOrDefault(it => it.LineFrom <= line && it.LineTo >= line);
     }
 }
---
+++ Tests/External/Plugins/ASCompletion.Tests/Generators/DocumentationGeneratorTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ASCompletion.Completion;
+using ASCompletion.Context;
+using ASCompletion.TestUtils;
+using NSubstitute;
+using NUnit.Framework;
+using PluginCore;
+using ScintillaNet;
+
+namespace ASCompletion.Generators
+{
+    public class DocumentationGeneratorTests : ASCompletionTests
+    {
+        [TestFixture]
+        public class ContextualGeneratorTests : DocumentationGeneratorTests
+        {
+            [TestFixtureSetUp]
+            public void SetUp()
+            {
+                var generator = new DocumentationGenerator();
+                ASContext.Context.DocumentationGenerator.Returns(generator);
+            }
+
+            public IEnumerable<TestCaseData> AS3TestCases
+            {
+                get { yield return null; }
+            }
+
+            [Test, TestCaseSource(nameof(AS3TestCases))]
+            public string AS3(string fileName, GeneratorJobType job, bool hasGenerator) => AS3Impl(sci, fileName, job, hasGenerator);
+
+            internal static string AS3Impl(ScintillaControl sci, string fileName, GeneratorJobType job, bool hasGenerator)
+            {
+                SetHaxeFeatures(sci);
+                var sourceText = ReadAllTextHaxe(fileName);
+                fileName = GetFullPathHaxe(fileName);
+                fileName = Path.GetFileNameWithoutExtension(fileName).Replace('.', Path.DirectorySeparatorChar) + Path.GetExtension(fileName);
+                fileName = Path.GetFullPath(fileName);
+                fileName = fileName.Replace($"\\FlashDevelop\\Bin\\Debug\\{nameof(ASCompletion)}\\Test_Files\\", $"\\Tests\\External\\Plugins\\{nameof(ASCompletion)}.Tests\\Test Files\\");
+                ASContext.Context.CurrentModel.FileName = fileName;
+                PluginBase.MainForm.CurrentDocument.FileName.Returns(fileName);
+                return Common(sci, sourceText, job, hasGenerator);
+            }
+
+            internal static string Common(ScintillaControl sci, string sourceText, GeneratorJobType job, bool hasGenerator)
+            {
+                SetSrc(sci, sourceText);
+                var options = new List<ICompletionListItem>();
+                ASContext.Context.DocumentationGenerator.ContextualGenerator(sci, sci.CurrentPos, options);
+                var item = options.Find(it => it is GeneratorItem && ((GeneratorItem)it).job == job);
+                if (hasGenerator)
+                {
+                    Assert.NotNull(item);
+                    var value = item.Value;
+                }
+                else Assert.IsNull(item);
+                return sci.Text;
+            }
+        }
+
+        protected static string ReadAllTextAS3(string fileName) => TestFile.ReadAllText(GetFullPathAS3(fileName));
+
+        protected static string GetFullPathAS3(string fileName) => $"ASCompletion.Test_Files.generated.as3.{fileName}.as";
+
+        protected static string ReadAllTextHaxe(string fileName) => TestFile.ReadAllText(GetFullPathHaxe(fileName));
+
+        protected static string GetFullPathHaxe(string fileName) => $"ASCompletion.Test_Files.generated.haxe.{fileName}.hx";
+    }
+}
