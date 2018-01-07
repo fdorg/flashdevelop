92826b4366190a89b1b499f956c53b219d4d13ad
Slava Buynov <slavab@playtika.com>
2018-01-07 14:04:45 +0300

    [Haxe][CodeGenerator] Added unit tests for #1743

--- Tests/External/Plugins/HaxeContext.Tests/Completion/CodeGeneratorTests.cs
+++ Tests/External/Plugins/HaxeContext.Tests/Completion/CodeGeneratorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Xml.Serialization;
 using ASCompletion.Completion;
 using ASCompletion.Context;
 using ASCompletion.Model;
@@ -18,7 +19,7 @@ namespace HaXeContext.Completion
         internal new static string ReadAllTextHaxe(string fileName) => TestFile.ReadAllText($"{nameof(HaXeContext)}.Test_Files.completion.generated.{fileName}.hx");
 
         [TestFixtureSetUp]
-        public void AddInterfaceDefTestsSetup()
+        public void Setup()
         {
             ASContext.Context.Settings.GenerateImports.Returns(true);
             ASContext.Context.SetHaxeFeatures();
@@ -30,43 +31,51 @@ namespace HaXeContext.Completion
             get
             {
                 yield return
-                    new TestCaseData("BeforeContextualGeneratorTests_issue1833_1")
+                    new TestCaseData("BeforeContextualGeneratorTests_issue1833_1", false)
                         .Returns(ReadAllTextHaxe("AfterContextualGeneratorTests_issue1833_1"))
                         .SetName("Issue1833. Case 1")
                         .SetDescription("https://github.com/fdorg/flashdevelop/issues/1833");
                 yield return
-                    new TestCaseData("BeforeContextualGeneratorTests_issue1833_2")
+                    new TestCaseData("BeforeContextualGeneratorTests_issue1833_2", false)
                         .Returns(ReadAllTextHaxe("AfterContextualGeneratorTests_issue1833_2"))
                         .SetName("Issue1833. Case 2")
                         .SetDescription("https://github.com/fdorg/flashdevelop/issues/1833");
+                yield return
+                    new TestCaseData("BeforeContextualGeneratorTests_issue1743_1", false)
+                        .Returns(ReadAllTextHaxe("AfterContextualGeneratorTests_issue1743_1"))
+                        .SetName("Issue1743. Case 1")
+                        .SetDescription("https://github.com/fdorg/flashdevelop/issues/1743");
+                yield return
+                    new TestCaseData("BeforeContextualGeneratorTests_issue1743_2", false)
+                        .Returns(ReadAllTextHaxe("AfterContextualGeneratorTests_issue1743_2"))
+                        .SetName("Issue1743. Case 2")
+                        .SetDescription("https://github.com/fdorg/flashdevelop/issues/1743");
+                yield return
+                    new TestCaseData("BeforeContextualGeneratorTests_issue1743_3", false)
+                        .Returns(ReadAllTextHaxe("AfterContextualGeneratorTests_issue1743_3"))
+                        .SetName("Issue1743. Case 3")
+                        .SetDescription("https://github.com/fdorg/flashdevelop/issues/1743");
             }
         }
 
         [Test, TestCaseSource(nameof(HaxeTestCases))]
-        public string Haxe(string fileName) => HaxeImpl(fileName, sci);
+        public string Haxe(string fileName, bool hasGenerator) => Impl(sci, fileName, hasGenerator);
 
-        internal static string HaxeImpl(string fileName, ScintillaControl sci)
+        internal static string Impl(ScintillaControl sci, string fileName, bool hasGenerator)
         {
             var sourceText = ReadAllTextHaxe(fileName);
             fileName = GetFullPathHaxe(fileName);
             ASContext.Context.CurrentModel.FileName = fileName;
             PluginBase.MainForm.CurrentDocument.FileName.Returns(fileName);
-            return Common(sourceText, sci);
+            return Common(sci, sourceText, hasGenerator);
         }
 
-        internal static string Common(string sourceText, ScintillaControl sci)
+        internal static string Common(ScintillaControl sci, string sourceText, bool hasGenerator)
         {
-            sci.Text = sourceText;
-            SnippetHelper.PostProcessSnippets(sci, 0);
-            var currentModel = ASContext.Context.CurrentModel;
-            new ASFileParser().ParseSrc(currentModel, sci.Text);
-            var line = sci.CurrentLine;
-            var currentClass = currentModel.Classes.Find(it => it.LineFrom <= line && it.LineTo >= line);
-            ASContext.Context.CurrentClass.Returns(currentClass);
-            var currentMember = currentClass.Members.Items.Find(it => it.LineFrom <= line && it.LineTo >= line);
-            ASContext.Context.CurrentMember.Returns(currentMember);
-            ASGenerator.contextToken = sci.GetWordFromPosition(sci.CurrentPos);
-            ASGenerator.ContextualGenerator(sci, new List<ICompletionListItem>());
+            SetSrc(sci, sourceText);
+            var options = new List<ICompletionListItem>();
+            ASGenerator.ContextualGenerator(sci, options);
+            if (!hasGenerator) Assert.AreEqual(0, options.Count);
             return sci.Text;
         }
     }
