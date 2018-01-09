8e8f0a8004486e7220b37c212d0757441c4492ab
Slava Buynov <slavab@playtika.com>
2018-01-09 14:14:02 +0300

    Fix typo...

--- External/Plugins/HaXeContext/Context.cs
+++ External/Plugins/HaXeContext/Context.cs
@@ -964,7 +964,7 @@ namespace HaXeContext
             if (token?.Length > 0)
             {
                 if (token.StartsWithOrdinal("0x")) return ResolveType("Int", inFile);
-                var first = token[0]
+                var first = token[0];
                 var last = token[token.Length - 1];
                 if (first == '[' && last == ']')
                 {
