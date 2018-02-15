429efc40ec03f633f3a196e8a7aff8f0d62e827b
SlavaRa <ratdg@ya.ru>
2018-02-15 09:33:46 +0300

    [CI][Tests] Add `--limit-output` to `cinst haxe`

--- CI/tests.ps1
+++ CI/tests.ps1
@@ -14,7 +14,7 @@ Param (
 
 If ($env:HAXEPATH -eq $null)
 {
-	cinst.exe haxe --version 3.4.3 -y
+	cinst.exe haxe --version 3.4.3 -y -r
 	$env:HAXEPATH = "c:\ProgramData\chocolatey\lib\haxe\"
 	RefreshEnv
 }
