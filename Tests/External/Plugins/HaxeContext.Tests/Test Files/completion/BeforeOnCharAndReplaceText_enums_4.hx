15bd6d53e590f6e9435bbb2d0b17e82e30dbc02e
SlavaRa <ratdg@ya.ru>
2018-06-14 09:13:26 +0300

    Test cases

---
+++ Tests/External/Plugins/HaxeContext.Tests/Test Files/completion/BeforeOnCharAndReplaceText_enums_4.hx
@@ -0,0 +1,10 @@
+﻿package;
+class Bar {
+	static function main() {
+		EAType.$(EntryPoint)
+	}
+}
+
+@:enum abstract EAType(String) {
+	var Value = "Value";
+}
---
+++ Tests/External/Plugins/HaxeContext.Tests/Test Files/completion/BeforeOnCharAndReplaceText_enums_6.hx
@@ -0,0 +1,10 @@
+﻿package;
+class Bar {
+	static function main() {
+		Value.$(EntryPoint)
+	}
+}
+
+@:enum abstract EAType(String) {
+	var Value = "Value";
+}
