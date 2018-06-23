cef3d239eb3bb5e3b89a66a2ff1a314aa51ab4e5
Tom Seddon <tom@ouro.ltd.uk>
2018-06-23 17:05:07 +0100

    Put the caret at the start of the found region before selecting it.
    Fix #2157.

--- FlashDevelop/Utilities/FRDialogGenerics.cs
+++ FlashDevelop/Utilities/FRDialogGenerics.cs
@@ -54,6 +54,7 @@ namespace FlashDevelop.Utilities
             Int32 end = start + sci.MBSafeTextLength(match.Value); // wchar to byte text length
             Int32 line = sci.LineFromPosition(start);
             sci.EnsureVisibleEnforcePolicy(line);
+            sci.SetSel(start, start);
             sci.SetSel(start, end);
         }
 
