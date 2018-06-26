715f0d7670b32a229fda225773723ea261e695c8
SlavaRa <ratdg@ya.ru>
2018-06-26 15:36:18 +0300

    [Haxe][CodeComplete] Improved inference the type of local variable

--- External/Plugins/HaXeContext/Completion/CodeComplete.cs
+++ External/Plugins/HaXeContext/Completion/CodeComplete.cs
@@ -254,10 +254,27 @@ namespace HaXeContext.Completion
             }
             var methodEndPosition = sci.LineEndPosition(ASContext.Context.CurrentMember.LineTo);
             var rvalueEnd = ExpressionEndPosition(sci, rvalueStart, sci.LineEndPosition(var.LineTo), true);
+            var parCount = 0;
+            var genCount = 0;
             for (var i = rvalueEnd; i < methodEndPosition; i++)
             {
                 if(sci.PositionIsOnComment(i) || sci.PositionIsInString(i)) continue;
                 var c = (char) sci.CharAt(i);
+                if (c == '(' && genCount == 0) parCount++;
+                else if (c == ')' && genCount == 0)
+                {
+                    parCount--;
+                    rvalueEnd = i + 1;
+                    if (parCount < 0) break;
+                }
+                else if (c == '<' && parCount == 0) genCount++;
+                else if (c == '>' && parCount == 0)
+                {
+                    genCount--;
+                    rvalueEnd = i + 1;
+                    if (genCount < 0) break;
+                }
+                else if (parCount > 0 || genCount > 0) continue;
                 if (c <= ' ') continue;
                 if (c == ';') break;
                 if (c == '.') rvalueEnd = ExpressionEndPosition(sci, i + 1, methodEndPosition);
