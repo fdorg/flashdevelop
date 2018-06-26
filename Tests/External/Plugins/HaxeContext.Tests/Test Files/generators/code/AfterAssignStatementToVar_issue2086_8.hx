9ce06b62c7ad7857144ddc984aace75ae729d7d4
SlavaRa <ratdg@ya.ru>
2018-06-26 15:43:01 +0300

    [Haxe][CodeComplete] Improved inference the type of local variable (#2166)

---
+++ Tests/External/Plugins/HaxeContext.Tests/Test Files/generators/code/AfterAssignStatementToVar_issue2086_8.hx
@@ -0,0 +1,9 @@
+﻿package;
+import flash.display.BitmapData;
+import haxe.ds.Vector;
+class Foo {
+	public function new() {
+		var v = new haxe.ds.Vector<flash.display.BitmapData>(Std.int(10));
+		var v:Vector<BitmapData> = v;
+	}
+}
