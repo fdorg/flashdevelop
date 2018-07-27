71c186eba994bc6386466888a1c5cd4cf0d23356
SlavaRa <ratdg@ya.ru>
2018-07-27 13:35:28 +0300

    Small cleanup...

--- External/Plugins/ASCompletion/Completion/ASGenerator.cs
+++ External/Plugins/ASCompletion/Completion/ASGenerator.cs
@@ -1928,8 +1928,6 @@ namespace ASCompletion.Completion
             sci.CurrentPos = bodyStart;
 
             var paramName = contextMember.Name;
-            var paramType = contextMember.Type;
-            paramType = ((ASGenerator) ASContext.Context.CodeGenerator).GetFieldTypeFromParameter(paramType, ref paramName);
 
             var varName = paramName;
             var scopedVarName = varName;
@@ -1978,23 +1976,10 @@ namespace ASCompletion.Completion
             sci.CurrentPos = position;
 
             var newMember = NewMember(varName, member, FlagType.Variable, scope);
-            newMember.Type = paramType;
+            newMember.Type = contextMember.Type;
 
             GenerateVariable(newMember, position, true);
             ASContext.Panel.RestoreLastLookupPosition();
-        }
-
-        protected virtual string GetFieldTypeFromParameter(string paramType, ref string paramName)
-        {
-            //foo(v1<generator>:Function/*(v1:Type):void*/)
-            if (paramType == ASContext.Context.Features.voidKey && (contextMember.Flags & FlagType.Function) != 0)
-                return $"Function/*({contextMember.ParametersString()}):{paramType}*/";
-            if (paramName.StartsWithOrdinal("..."))
-            {
-                paramName = paramName.TrimStart('.');
-                return "Array";
-            }
-            return paramType;
         }
 
         /// <summary>
