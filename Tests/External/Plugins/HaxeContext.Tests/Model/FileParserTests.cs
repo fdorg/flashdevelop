9d82059f85f4c88543b88f3e4bc9dc907353efe3
SlavaRa <ratdg@ya.ru>
2018-08-09 19:45:56 +0300

    Added tests...

--- Tests/External/Plugins/HaxeContext.Tests/Model/FileParserTests.cs
+++ Tests/External/Plugins/HaxeContext.Tests/Model/FileParserTests.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using ASCompletion;
 using ASCompletion.Context;
+using ASCompletion.Model;
 using HaXeContext.TestUtils;
 using NUnit.Framework;
 
@@ -41,6 +42,44 @@ namespace HaXeContext.Model
         {
             var model = ASContext.Context.GetCodeModel(ReadAllText(fileName));
             return model.Classes.First().Members.Items.First().Parameters.Count;
+        }
+
+        static IEnumerable<TestCaseData> Issue2210TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("Issue2210_1")
+                    .Returns(new MemberModel("foo", "Int", FlagType.Dynamic | FlagType.Function, Visibility.Private)
+                    {
+                        LineFrom = 2,
+                        LineTo = 2,
+                    })
+                    .SetName("function foo() return 1;")
+                    .SetDescription("https://github.com/fdorg/flashdevelop/issues/2210");
+                yield return new TestCaseData("Issue2210_2")
+                    .Returns(new MemberModel("bar", "Int", FlagType.Dynamic | FlagType.Function, Visibility.Private)
+                    {
+                        LineFrom = 4,
+                        LineTo = 4,
+                    })
+                    .SetName("function foo() return 1;\nfunction bar() return 1;")
+                    .SetDescription("https://github.com/fdorg/flashdevelop/issues/2210");
+                yield return new TestCaseData("Issue2210_3")
+                    .Returns(new MemberModel("foo", "Int", FlagType.Dynamic | FlagType.Function, Visibility.Private)
+                    {
+                        LineFrom = 2,
+                        LineTo = 5,
+                    })
+                    .SetName("function foo(i:Int):Int\nreturn i % 2 == 0\n? 0\n: 1;")
+                    .SetDescription("https://github.com/fdorg/flashdevelop/issues/2210");
+            }
+        }
+
+        [Test, TestCaseSource(nameof(Issue2210TestCases))]
+        public MemberModel ParseFile_Issue2210(string fileName)
+        {
+            var model = ASContext.Context.GetCodeModel(ReadAllText(fileName));
+            return model.Classes.First().Members.Items.Last();
         }
     }
 }
