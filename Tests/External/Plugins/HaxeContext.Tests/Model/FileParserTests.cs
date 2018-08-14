1f4d4c0207d9f7c6f88389c547cb34cc89a683df
SlavaRa <ratdg@ya.ru>
2018-08-14 18:00:27 +0300

    Added tests...

--- Tests/External/Plugins/HaxeContext.Tests/Model/FileParserTests.cs
+++ Tests/External/Plugins/HaxeContext.Tests/Model/FileParserTests.cs
@@ -209,6 +209,22 @@ namespace HaXeContext.Model
                     })
                     .SetName("function foo(i:Int):Int trace(i)\vprivate var v:Int;")
                     .SetDescription("https://github.com/fdorg/flashdevelop/issues/2210");
+                yield return new TestCaseData("Issue2210_12")
+                    .Returns(new List<MemberModel>
+                    {
+                        new MemberModel("foo", "Int", FlagType.Dynamic | FlagType.Function, Visibility.Private)
+                        {
+                            LineFrom = 2,
+                            LineTo = 2,
+                        },
+                        new MemberModel("v", "Int", FlagType.Access | FlagType.Dynamic | FlagType.Variable, Visibility.Private)
+                        {
+                            LineFrom = 3,
+                            LineTo = 3,
+                        }
+                    })
+                    .SetName("function foo(i:Int):Int trace(i)\vprivate var v:Int;")
+                    .SetDescription("https://github.com/fdorg/flashdevelop/issues/2210");
             }
         }
 
