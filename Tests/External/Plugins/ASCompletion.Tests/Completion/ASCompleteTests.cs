67e536b94d7c048950b239d76cd71e14119bb498
SlavaRa <ratdg@ya.ru>
2018-10-27 16:51:55 +0300

    More test...

--- Tests/External/Plugins/ASCompletion.Tests/Completion/ASCompleteTests.cs
+++ Tests/External/Plugins/ASCompletion.Tests/Completion/ASCompleteTests.cs
@@ -1422,6 +1422,10 @@ namespace ASCompletion.Completion
                         .Returns(new ClassModel {Name = "Bool", Flags = FlagType.Class})
                         .SetName("foo<T>(_, bool, ?_, ?_).<complete> Issue 2203. Case 2")
                         .SetDescription("https://github.com/fdorg/flashdevelop/issues/2203");
+                    yield return new TestCaseData(ReadAllText("GetExpressionType_ParameterizedFunction_issue2203_3"))
+                        .Returns(new ClassModel {Name = "Bool", Flags = FlagType.Class})
+                        .SetName("foo<K, T, R1, R2>(_, 1.0, ?_, ?_).<complete> Issue 2203. Case 3")
+                        .SetDescription("https://github.com/fdorg/flashdevelop/issues/2203");
                 }
             }
 
--- Tests/External/Plugins/ASCompletion.Tests/Test Files/completion/haxe/GetExpressionType_ParameterizedFunction_issue2203_2.hx
+++ Tests/External/Plugins/ASCompletion.Tests/Test Files/completion/haxe/GetExpressionType_ParameterizedFunction_issue2203_2.hx
@@ -1,5 +1,5 @@
 package;
-public class GetExpressionType_ParameterizedFunction_issue2203_1 {
+public class GetExpressionType_ParameterizedFunction_issue2203_2 {
 	public static function foo<T>(i:Int, v:T, ?a, ?b):T {
 		foo( 10 , true ).$(EntryPoint)
 	}
