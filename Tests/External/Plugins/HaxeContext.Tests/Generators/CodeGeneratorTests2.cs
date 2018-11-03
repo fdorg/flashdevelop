3e25b5a5462baf612f987c990cc15ac74526f72c
SlavaRa <ratdg@ya.ru>
2018-11-03 16:35:09 +0300

    Cleanup...

--- Tests/External/Plugins/HaxeContext.Tests/Generators/CodeGeneratorTests2.cs
+++ Tests/External/Plugins/HaxeContext.Tests/Generators/CodeGeneratorTests2.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using ASCompletion.Completion;
 using ASCompletion.Context;
-using ASCompletion.Model;
 using NSubstitute;
 using NUnit.Framework;
 using PluginCore;
@@ -240,25 +239,6 @@ namespace HaXeContext.Generators
         {
             SetSrc(sci, CodeGeneratorTests.ReadAllText(fileName));
             CodeGeneratorTests.SetCurrentFile(fileName);
-            var context = (Context) ASContext.GetLanguageContext("haxe");
-            context.CurrentModel = ASContext.Context.CurrentModel;
-            context.completionCache.IsDirty = true;
-            context.GetTopLevelElements();
-            ASContext.Context
-                .When(it => it.ResolveTopLevelElement(Arg.Any<string>(), Arg.Any<ASResult>()))
-                .Do(it =>
-                {
-                    var token = it.ArgAt<string>(0);
-                    var result = it.ArgAt<ASResult>(1);
-                    if (token == "this")
-                    {
-                        result.Type = ASContext.Context.CurrentClass;
-                        result.InFile = ASContext.Context.CurrentModel;
-                        result.InClass = ASContext.Context.CurrentClass;
-                        result.Member = new MemberModel("this", ASContext.Context.CurrentClass.Type, FlagType.Variable | FlagType.Intrinsic, Visibility.Public);
-                    }
-                    else context.ResolveTopLevelElement(token, result);
-                });
             var options = new List<ICompletionListItem>();
             ASGenerator.ContextualGenerator(sci, options);
             if (hasGenerator)
