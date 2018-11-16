33bf5874bacc2ef15d6cbe13b80b000e75ec2a16
SlavaIvanov <slava_ivanov@belkatechnologies.com>
2018-11-16 21:29:17 +0300

    Replace 'Array.IndexOf(array, item) != -1' by 'array.Contains(item)'

--- External/Plugins/HaXeContext/Completion/CodeComplete.cs
+++ External/Plugins/HaXeContext/Completion/CodeComplete.cs
@@ -266,7 +266,7 @@ namespace HaXeContext.Completion
                     var names = @params["Default"]
                         .Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries)
                         .Select(it => it.Trim()).ToArray();
-                    if (names.Length != 0) list.Items.RemoveAll(it => Array.IndexOf(names, it.Name) != -1);
+                    if (names.Length != 0) list.Items.RemoveAll(it => names.Contains(it.Name));
                 }
                 if (list.Count > 0) CompletionList.Show(list.Items.Select(it => new MemberItem(it)).ToList<ICompletionListItem>(), autoHide);
                 return true;
