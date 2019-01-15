06575ea8cc7cda5c4235a6441427696f06b345e1
SlavaRa <ratdg@ya.ru>
2019-01-15 15:36:12 +0300

    [Haxe][CodeComplete] Improved Inference the type of function. wip #170

--- Tests/External/Plugins/HaxeContext.Tests/Completion/CodeCompleteTests.cs
+++ Tests/External/Plugins/HaxeContext.Tests/Completion/CodeCompleteTests.cs
@@ -1103,6 +1103,10 @@ namespace HaXeContext.Completion
                     .Returns(CodeCompleteTests.ReadAllText("AfterOnCharAndReplaceText_issue170_5"))
                     .SetName("foo<T>([]).<complete> Issue 170. Case 5")
                     .SetDescription("https://github.com/fdorg/flashdevelop/issues/170");
+                yield return new TestCaseData("BeforeOnChar_issue170_6", '.', false)
+                    .Returns(CodeCompleteTests.ReadAllText("AfterOnCharAndReplaceText_issue170_6"))
+                    .SetName("foo<T>([1,2,3,4]).<complete> Issue 170. Case 6")
+                    .SetDescription("https://github.com/fdorg/flashdevelop/issues/170");
             }
         }
 
