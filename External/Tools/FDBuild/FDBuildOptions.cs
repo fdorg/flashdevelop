ba7dac6b6c2282ba7b2b5e0604b5f3e6ab184c34
Slava Ivanov <slava_ivanov@belkatechnologies.com>
2019-06-05 18:34:01 +0300

    Fix typo...

--- External/Tools/FDBuild/FDBuildOptions.cs
+++ External/Tools/FDBuild/FDBuildOptions.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System.Collections.Generic;
 using Mono.GetOptions;
 using System.IO;
 
--- External/Tools/FDBuild/Mono/GetOptions/OptionList.cs
+++ External/Tools/FDBuild/Mono/GetOptions/OptionList.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Mono.GetOptions
 {
     using Mono;
