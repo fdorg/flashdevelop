6d3827d77a8890cfe16e51e2822bc41d6a9ee1e9
slavara <ratdg@ya.ru>
2019-12-31 21:37:47 +0300

    [Refactoring] ClassModelExtensions.GetMembers() -> ClassModelExtensions.SearchMembers()

--- External/Plugins/ASCompletion/Model/ClassModelExtensions.cs
+++ External/Plugins/ASCompletion/Model/ClassModelExtensions.cs
@@ -4,14 +4,20 @@ namespace ASCompletion.Model
 {
     public static class ClassModelExtensions
     {
-        public static MemberList GetMembers(this ClassModel @this, FlagType flags, bool recursive)
+        /// <summary>
+        /// Return all MemberModel instance matches in the ClassModel's members
+        /// </summary>
+        /// <param name="mask">Flags mask</param>
+        /// <param name="recursive"></param>
+        /// <returns>All matches</returns>
+        public static MemberList SearchMembers(this ClassModel @this, FlagType flags, bool recursive)
         {
-            if (!recursive) @this.GetMembers(flags);
+            if (!recursive) @this.SearchMembers(flags);
             if (@this.Extends.IsVoid()) @this.ResolveExtends();
             var type = @this.Extends;
             while (!type.IsVoid())
             {
-                var result = type.GetMembers(flags);
+                var result = type.SearchMembers(flags);
                 if (result.Count > 0) return result;
                 type = type.Extends;
             }
@@ -19,11 +25,31 @@ namespace ASCompletion.Model
         }
 
         /// <summary>
-        /// Return all MemberModel instance matches in the ClassModel
+        /// Return all MemberModel instance matches in the ClassModel's members
         /// </summary>
         /// <param name="mask">Flags mask</param>
         /// <returns>All matches</returns>
-        static MemberList GetMembers(this ClassModel @this, FlagType mask) => new MemberList(@this.Members.Where(it => (it.Flags & mask) == mask));
+        static MemberList SearchMembers(this ClassModel @this, FlagType mask) => new MemberList(@this.Members.Where(it => (it.Flags & mask) == mask));
+
+        /// <summary>
+        /// Return the first MemberModel instance match in the ClassModel's members
+        /// </summary>
+        /// <param name="name">Member name to mach</param>
+        /// <param name="recursive"></param>
+        /// <returns>First match</returns>
+        public static MemberModel SearchMember(this ClassModel @this, string name, bool recursive)
+        {
+            if (!recursive) return @this.Members.Search(name, 0, 0);
+            if (@this.Extends.IsVoid()) @this.ResolveExtends();
+            var type = @this;
+            while (!type.IsVoid())
+            {
+                var result = type.Members.Search(name, 0, 0);
+                if (result != null) return result;
+                type = type.Extends;
+            }
+            return null;
+        }
 
         public static bool ContainsMember(this ClassModel @this, FlagType flags, bool recursive)
         {
