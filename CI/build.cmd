6961eec81e4fcebb41b231dfab085353fef5dc43
slavara <ratdg@ya.ru>
2020-01-20 17:09:51 +0300

    Cleanup...

--- CI/build.cmd
+++ CI/build.cmd
@@ -26,6 +26,8 @@ if %errorlevel% neq 0 goto :error
 del "FlashDevelop\Bin/Debug\*.Tests.*" /Q
 del "FlashDevelop\Bin/Debug\NSubstitute.*" /Q
 del "FlashDevelop\Bin/Debug\nunit.framework.*" /Q
+del "FlashDevelop\Bin/Debug\Castle.Core.*" /Q
+del "FlashDevelop\Bin/Debug\System.Threading.Extensions.*" /Q
 
 :: Check if the build was triggered by a pull request
 if "%APPVEYOR_PULL_REQUEST_NUMBER%" neq "" (
