788093aefc5d1d0a034341bebdb535ead51084c4
slavara <ratdg@ya.ru>
2020-03-18 21:58:41 +0300

    Minor cleanup...

--- CI/build.cmd
+++ CI/build.cmd
@@ -1,9 +1,9 @@
 :: Builds the binary on the server for CI
 
 :: Set paths
-set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319\
-set PATH=%PATH%;C:\Program Files (x86)\Git\bin\
-set PATH=%PATH%;C:\Program Files (x86)\NSIS
+:: set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319\
+:: set PATH=%PATH%;C:\Program Files (x86)\Git\bin\
+:: set PATH=%PATH%;C:\Program Files (x86)\NSIS
 set PATH=%PATH%;C:\Program Files\7-Zip\
 
 :flashdevelop
