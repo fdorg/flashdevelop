79ea9b1e7338e25af28a8cadb5c80d0520bef284
slavara <ratdg@ya.ru>
2020-12-21 02:01:19 +0300

    WIP

--- CI/tests.ps1
+++ CI/tests.ps1
@@ -15,13 +15,7 @@ Param (
 If ($env:HAXEPATH -eq $null)
 {
 	cinst.exe haxe --version 4.1.4 -y --no-progress
-    $paths = [System.IO.Directory]::GetDirectories("C:\ProgramData\chocolatey\lib\haxe", "haxe_*");
-    Write-Output "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    Get-ChildItem -Path "C:\ProgramData\chocolatey\lib\haxe"
-    Write-Output "paths: " + $paths
-    $env:HAXEPATH = $paths[0];
-    Write-Output "env:HAXEPATH: " + $env:HAXEPATH
-    Get-ChildItem -Path $env:HAXEPATH
+    $env:HAXEPATH = [System.IO.Directory]::GetDirectories("C:\ProgramData\chocolatey\lib\haxe", "haxe_*")[0];
 }
 
 If ((Get-Command "nunit3-console.exe" -ErrorAction SilentlyContinue) -ne $null)
@@ -36,8 +30,8 @@ If ((Get-Command "nunit3-console.exe" -ErrorAction SilentlyContinue) -ne $null)
 
     cd $path
     #nunit3-console.exe $testFiles --result=myresults.xml;format=AppVeyor
-    nunit3-console.exe $testFiles --x86
-    #nunit3-console.exe $testFiles
+    #nunit3-console.exe $testFiles --x86
+    nunit3-console.exe $testFiles
 
     #It turns out it's not needed to upload the file
     #if ((Test-Path env:\APPVEYOR_JOB_ID) -And (Test-Path TestResult.xml))
