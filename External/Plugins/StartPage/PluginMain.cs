3000c1005e6377d898fb98c1a41bc90c1fef688f
slavara <ratdg@ya.ru>
2021-01-04 17:09:01 +0300

    Fix RTE

--- External/Plugins/StartPage/PluginMain.cs
+++ External/Plugins/StartPage/PluginMain.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Web;
@@ -18,8 +17,8 @@ namespace StartPage
     public class PluginMain : IPlugin
     {
         bool justOpened = true;
-        string defaultRssUrl = "";
-        string defaultStartPageUrl = "";
+        string defaultRssUrl = string.Empty;
+        string defaultStartPageUrl = string.Empty;
         StartPageWebBrowser startPageWebBrowser;
         string settingFilename;
         Settings settingObject;
@@ -90,27 +89,30 @@ namespace StartPage
         {
             switch (e.Type)
             {
-                case EventType.Command :
+                case EventType.Command:
                     var de = (DataEvent)e;
                     switch (de.Action)
                     {
-                        case ProjectManagerEvents.Project :
+                        case ProjectManagerEvents.Project:
                             // Close pluginPanel if the user has the setting checked and a project is opened
                             if (de.Data != null && startPage != null)
                             {
                                 if (settingObject.CloseOnProjectOpen) startPage.Close();
                                 // The project manager does not update recent projects until after
                                 // it broadcasts this event so we'll wait a little bit before refreshing
-                                Timer timer = new Timer();
-                                timer.Interval = 100;
-                                timer.Tick += delegate { timer.Stop(); if (!startPageWebBrowser.IsDisposed) startPageWebBrowser.SendProjectInfo(); };
+                                var timer = new Timer {Interval = 100};
+                                timer.Tick += (_, _) =>
+                                {
+                                    timer.Stop();
+                                    if (!startPageWebBrowser.IsDisposed) startPageWebBrowser.SendProjectInfo();
+                                };
                                 timer.Start();
                             }
                             break;
                     }
                     break;
 
-                case EventType.FileEmpty :
+                case EventType.FileEmpty:
                     if ((justOpened & (settingObject.ShowStartPageOnStartup == ShowStartPageOnStartupEnum.Always || settingObject.ShowStartPageOnStartup == ShowStartPageOnStartupEnum.NotRestoringSession)) || settingObject.ShowStartPageInsteadOfUntitled)
                     {
                         ShowStartPage();
@@ -139,15 +141,18 @@ namespace StartPage
         /// <summary>
         /// Initializes important variables
         /// </summary>
-        public void InitBasics()
+        void InitBasics()
         {
-            int length = DistroConfig.DISTRIBUTION_NAME.Length + 1;
-            string dataDir = Path.Combine(PathHelper.DataDir, "StartPage");
-            string localeName = PluginBase.Settings.LocaleVersion.ToString();
-            string version = Application.ProductName.Substring(length, Application.ProductName.IndexOfOrdinal(" for") - length);
-            string fileWithArgs = "index.html?l=" + localeName + "&v=" + HttpUtility.HtmlEncode(version);
+            var length = DistroConfig.DISTRIBUTION_NAME.Length + 1;
+            var localeName = PluginBase.Settings.LocaleVersion.ToString();
+            var productName = Application.ProductName;
+            var version = productName.Contains(" for", out var index)
+                ? productName.Substring(length, index - length)
+                : productName;
+            var fileWithArgs = $"index.html?l={localeName}&v={HttpUtility.HtmlEncode(version)}";
             defaultStartPageUrl = Path.Combine(PathHelper.AppDir, "StartPage", fileWithArgs);
             defaultRssUrl = DistroConfig.DISTRIBUTION_RSS; // Default feed...
+            var dataDir = Path.Combine(PathHelper.DataDir, nameof(StartPage));
             if (!Directory.Exists(dataDir)) Directory.CreateDirectory(dataDir);
             settingFilename = Path.Combine(dataDir, "Settings.fdb");
             Description = TextHelper.GetString("Info.Description");
@@ -165,7 +170,7 @@ namespace StartPage
         /// <summary>
         /// Loads the plugin settings
         /// </summary>
-        public void LoadSettings()
+        void LoadSettings()
         {
             settingObject = new Settings();
             if (!File.Exists(settingFilename)) SaveSettings();
@@ -175,12 +180,12 @@ namespace StartPage
         /// <summary>
         /// Saves the plugin settings
         /// </summary>
-        public void SaveSettings() => ObjectSerializer.Serialize(settingFilename, settingObject);
+        void SaveSettings() => ObjectSerializer.Serialize(settingFilename, settingObject);
 
         /// <summary>
         /// Creates a menu item for the plugin
         /// </summary>
-        public void CreateMenuItem()
+        void CreateMenuItem()
         {
             var title = TextHelper.GetString("Label.ViewMenuItem");
             var menu = (ToolStripMenuItem)PluginBase.MainForm.FindMenuItem("ViewMenu");
@@ -192,20 +197,26 @@ namespace StartPage
         /// <summary>
         /// Creates the Start Page Tab
         /// </summary>
-        public void CreateStartPage()
+        void CreateStartPage()
         {
             startPageWebBrowser = new StartPageWebBrowser(CurrentPageUrl, CurrentRssUrl);
             startPage = PluginBase.MainForm.CreateCustomDocument(startPageWebBrowser);
             startPage.Icon = ImageKonverter.ImageToIcon(pluginImage);
-            startPage.Disposed += PluginPanelDisposed;
-            startPage.Closing += PluginPanelClosing; 
+            startPage.Disposed += (_, _) => startPage = null;
+            startPage.Closing += (_, e) =>
+            {
+                if (settingObject.ShowStartPageInsteadOfUntitled && PluginBase.MainForm.Documents.Length == 1)
+                {
+                    e.Cancel = true;
+                }
+            }; 
             startPage.Text = TextHelper.GetString("Title.StartPage");
         }
 
         /// <summary>
         /// Shows the Start Page Tab and creates if necessary.
         /// </summary>
-        public void ShowStartPage()
+        void ShowStartPage()
         {
             if (startPage is null) CreateStartPage();
             else startPage.Show(PluginBase.MainForm.DockPanel);
@@ -219,22 +230,6 @@ namespace StartPage
         /// Shows the start page.
         /// </summary>
         void ViewMenuClick(object sender, EventArgs e) => ShowStartPage();
-
-        /// <summary>
-        /// Some internal event handling for closing.
-        /// </summary>
-        void PluginPanelClosing(object sender, CancelEventArgs e)
-        {
-            if (settingObject.ShowStartPageInsteadOfUntitled && PluginBase.MainForm.Documents.Length == 1)
-            {
-                e.Cancel = true;
-            }
-        }
-
-        /// <summary>
-        /// Reset the start page reference.
-        /// </summary>
-        void PluginPanelDisposed(object sender, EventArgs e) => startPage = null;
 
         #endregion
     }
