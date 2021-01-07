cf62597ea7f748097056ddfb9541abb40f934b1f
slavara <ratdg@ya.ru>
2021-01-07 15:51:30 +0300

    WIP

--- External/Plugins/OutputPanel/PluginUI.cs
+++ External/Plugins/OutputPanel/PluginUI.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
 using System.Text.RegularExpressions;
--- External/Plugins/ProjectManager/Helpers/TreeViews/MultiSelectTreeView.cs
+++ External/Plugins/ProjectManager/Helpers/TreeViews/MultiSelectTreeView.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
-using PluginCore;
 
 namespace System.Windows.Forms
 {
