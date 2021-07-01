37e3c58a017c35df5e5924710d83dcb5d2aeef93
SlavaRa <ratdg@ya.ru>
2021-07-01 20:10:28 +0300

    [Regression][PreviousEdit] Fix build configurations (#3260)

--- PluginCore/PluginCore/DistroConfig.cs
+++ PluginCore/PluginCore/DistroConfig.cs
@@ -8,7 +8,7 @@
         public const string DISTRIBUTION_HELP = "https://www.flashdevelop.org/wikidocs/";
         public const string DISTRIBUTION_VERSION = "https://www.flashdevelop.org/latest.txt";
         public const string DISTRIBUTION_ABOUT = "FlashDevelop logo, domain and the name are copyright of Mika Palmu.\r\nDevelopment: Mika Palmu, Philippe Elsass and all helpful contributors.";
-        public const string DISTRIBUTION_COPYRIGHT = "FlashDevelop.org 2005-2020";
+        public const string DISTRIBUTION_COPYRIGHT = "FlashDevelop.org 2005-2021";
         public const string DISTRIBUTION_COMPANY = "FlashDevelop.org";
         public const string DISTRIBUTION_RSS = "https://www.flashdevelop.org/community/rss.php?f=15";
         public const string DISTRIBUTION_EXT = "as";
